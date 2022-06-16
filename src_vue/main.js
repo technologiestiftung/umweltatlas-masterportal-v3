4f173d383a3e73f16dbc0a581f2f1bee8bf6aa76
sven_beyel <sven.beyel@gv.hamburg.de>
2022-06-16 14:19:33 +0200

    add the plugins vuetify, remoteInterface, addons and i18next to src_vue
    in this case the legacy code ist removed

--- src_vue/main.js
+++ src_vue/main.js
@@ -8,6 +8,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./app-store";
 
+import loadAddons from "./plugins/addons";
+import remoteInterface from "./plugins/remoteInterface";
+import {instantiateVuetify} from "./plugins/vuetify";
+import {initiateVueI18Next, initLanguage} from "./plugins/i18next";
+
 /* eslint-disable no-process-env */
 if (process.env.NODE_ENV === "development") {
     Vue.config.devtools = true;
@@ -27,11 +32,21 @@ const configPath = window.location.pathname.substring(0, window.location.pathnam
 
 // Wait until config.js is loaded
 loadConfigJs.then(() => {
+    initLanguage(Config.portalLanguage);
+    loadAddons(Config.addons);
+
+    // Load remoteInterface
+    if (Object.prototype.hasOwnProperty.call(Config, "remoteInterface")) {
+        Vue.use(remoteInterface, Config.remoteInterface);
+    }
+
     const app = new Vue({
         el: "#masterportal-root",
         name: "VueApp",
         render: h => h(App),
-        store
+        store,
+        i18n: initiateVueI18Next(),
+        vuetify: instantiateVuetify()
     });
 
     app.$mount();
