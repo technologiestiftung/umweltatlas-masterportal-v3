42c9d854de0cac1a84380afd4ea8c948042fa184
sven_beyel <sven.beyel@gv.hamburg.de>
2022-06-16 11:12:47 +0200

    add basic main functions for vue and vuex

---
+++ src_vue/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "App"
+};
+</script>
+
+<template>
+    <div
+        id="masterportal-container"
+        class="masterportal-container"
+    >
+        <!-- do something -->
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
