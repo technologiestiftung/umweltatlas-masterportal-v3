09e7b80f941ce78758e94bf108bba0f266ca2b2b
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-08-10 10:48:31 +0200

    add a simple portal master_3_0_0
    that contains a simple config to start migration to MP 3.0.0

---
+++ portal/master_3_0_0/config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+const Config = {
+    namedProjections: [
+        // GK DHDN
+        ["EPSG:31467", "+title=Bessel/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+        // ETRS89 UTM
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+        // LS 320: zusammen mit Detlef Koch eingepflegt und geprüft
+        ["EPSG:8395", "+title=ETRS89/Gauß-Krüger 3 +proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=GRS80 +datum=GRS80 +units=m +no_defs"],
+        // WGS84
+        ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"]
+    ],
+    layerConf: "https://geodienste.hamburg.de/services-internet.json",
+    restConf: "https://geodienste.hamburg.de/lgv-config/rest-services-internet.json"
+};
