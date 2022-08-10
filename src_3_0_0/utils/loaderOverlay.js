f60bf6b6708b2c6af78d9b8e3340f9e917faf22a
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-08-10 09:40:38 +0200

    remove LoaderOverlay show function in vue_3_0_0

--- src_3_0_0/utils/loaderOverlay.js
+++ src_3_0_0/utils/loaderOverlay.js
@@ -33,6 +33,7 @@ export default {
         }
         return this.loaderOverlayCount;
     },
+
     /**
      * Initiates the fade animation of MP logo and title. Ths may be interrupted by a mousedown event.
      * @returns {void}
@@ -59,6 +60,7 @@ export default {
 
         setTimeout(this.cleanup.bind(this), 3400);
     },
+
     /**
      * Removes portal logos and titles from Loader, also removes animation classes.
      * @returns {void}
