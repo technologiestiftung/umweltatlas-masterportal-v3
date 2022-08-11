df7b74313c9a85225179bdd7f08afb96cf62076b
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-08-11 11:45:32 +0200

    add LayerFactory and layerCollection and start create layers of type wms
    BG-3029

--- src_3_0_0/core/layers/layerOl2dRasterWms.js
+++ src_3_0_0/core/layers/layerOl2dRasterWms.js
@@ -1,1 +1,18 @@
 
+import LayerOl2dRaster from "./layerOl2dRaster";
+
+/**
+ * Creates a ol 2d raster layer.
+ * @param {Object} attributes Attributes of the layer.
+ * @returns {void}
+ */
+export default function LayerOl2dRasterWms (attributes) {
+    const defaultAttributes = {
+        infoFormat: "text/xml"
+    };
+
+    this.attributes = Object.assign(defaultAttributes, attributes);
+    LayerOl2dRaster.call(this, this.attributes);
+}
+
+LayerOl2dRasterWms.prototype = Object.create(LayerOl2dRaster.prototype);
