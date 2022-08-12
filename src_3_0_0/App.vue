77c6349a2839e68de8b1490417bd97da951cdb85
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-08-12 18:31:11 +0200

    provide filtered raw layers if treetype default
    BG3028

--- src_3_0_0/App.vue
+++ src_3_0_0/App.vue
@@ -36,9 +36,6 @@ export default {
         this.loadConfigsToTheVuexState();
         this.checkVueObservation();
     },
-    mounted () {
-        LoaderOverlay.hide();
-    },
     methods: {
         ...mapActions([
             "extendLayers",
