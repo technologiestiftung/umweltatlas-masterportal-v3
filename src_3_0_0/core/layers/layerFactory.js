7fc7ac957c16f2073115df210b612158bc530f7e
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-08-18 11:29:04 +0200

    add layer type geojson
    refactor other vectorlayers in this case
    
    BG-3034

--- src_3_0_0/core/layers/layerFactory.js
+++ src_3_0_0/core/layers/layerFactory.js
@@ -1,9 +1,9 @@
 import store from "../../app-store";
 import layerCollection from "./layerCollection";
-import Layer2dRasterWms from "./layer2dRasterWms";
 import Layer2dVectorGeojson from "./layer2dVectorGeojson";
 import Layer2dVectorVectorbase from "./Layer2dVectorVectorbase";
 import Layer2dVectorWfs from "./layer2dVectorWfs";
+import Layer2dRasterWms from "./layer2dRasterWms";
 
 const possibleLayerTypes = {
     GEOJSON: Layer2dVectorGeojson,
