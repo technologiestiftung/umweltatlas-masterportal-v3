4ac9c5067f4f732a2c404fe0d2d02aa9ddb2f7a4
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2022-10-18 07:47:05 +0200

    update unit test for mapInteractions
    BG-3394

---
+++ src_3_0_0/core/maps/tests/unit/store/actionsMapsInteractions.spec.js
@@ -0,0 +1,74 @@
+import {expect} from "chai";
+import Map from "ol/Map";
+import sinon from "sinon";
+import View from "ol/View";
+
+import actions from "../../../store/actionsMapsInteractions";
+
+const {
+    registerListener,
+    resetView,
+    unregisterListener
+} = actions;
+
+describe("src_3_0_0/core/maps/store/actionsMapsInteractions.js", () => {
+    let olMap,
+        payload;
+
+    before(() => {
+        olMap = new Map({
+            id: "ol",
+            mode: "2D",
+            view: new View({
+                center: [10, 20],
+                resolution: 2.6458319045841048,
+                rotation: 1,
+                zoom: 3
+            })
+        });
+        payload = {
+            type: "pointermove",
+            listener: "updatePointer",
+            listenerType: "dispatch"
+        };
+        mapCollection.clear();
+        mapCollection.addMap(olMap, "2D");
+    });
+
+    after(() => {
+        sinon.restore();
+    });
+
+    describe("registerListener", () => {
+        it("register pointermove listener to ol map", () => {
+            const dispatch = sinon.spy(),
+                commit = sinon.spy();
+
+            registerListener({commit, dispatch}, payload);
+            expect(Object.keys(olMap.listeners_)).include("pointermove");
+        });
+    });
+
+    describe("resetView", () => {
+        it("should reset the map view", () => {
+            const state = {
+                initialCenter: [50, 50],
+                initialRotation: 0,
+                initialZoom: 5
+            };
+
+            resetView({state});
+
+            expect(mapCollection.getMapView("2D").getCenter()).to.deep.equals([50, 50]);
+            expect(mapCollection.getMapView("2D").getRotation()).to.equals(0);
+            expect(mapCollection.getMapView("2D").getZoom()).to.equals(5);
+        });
+    });
+
+    describe("unregisterListener", () => {
+        it("unregister pointermove listener from ol map", () => {
+            unregisterListener({}, payload);
+            expect(Object.keys(olMap.listeners_)).not.include("pointermove");
+        });
+    });
+});
