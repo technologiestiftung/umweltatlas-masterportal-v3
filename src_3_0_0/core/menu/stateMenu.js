0d062aff7b218e1b7c207cdd927c2658a49fb8f8
Pascal Röhling <pascal.roehling@dataport.de>
2022-10-20 20:05:50 +0200

    Remove unused parameters from JSDoc

--- src_3_0_0/core/menu/stateMenu.js
+++ src_3_0_0/core/menu/stateMenu.js
@@ -5,8 +5,6 @@ import ScaleSwitcher from "./modules/scaleSwitcher/components/ScaleSwitcher.vue"
  * Menu type definition
  * @typedef {Object} menu
  * @property {Object} componentMap Maps config.json menu key to component.
- * @property {Object} identifyComponentByProperty Maps config.json menu entries by the existence of a defined property to a component.
- * @property {Array} menuItems holds all menu items to display in the menu.
  */
 
 export default {
