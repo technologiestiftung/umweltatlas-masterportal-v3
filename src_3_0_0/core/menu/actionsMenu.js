48e213115e4c2bfdeebe7943fcd4ba6d56138525
Pascal Röhling <pascal.roehling@dataport.de>
2022-10-26 19:17:27 +0200

    Add JSDoc to the actions of the menu
    # Conflicts:
    #	src_3_0_0/core/menu/actionsMenu.js

--- src_3_0_0/core/menu/actionsMenu.js
+++ src_3_0_0/core/menu/actionsMenu.js
@@ -1,6 +1,15 @@
 import Vue from "vue";
 
 export default {
+    /**
+     * Action triggered when a menu element has been clicked.
+     * Add an entry to the navigation and, when the element
+     * was a Folder, focus the first child-element, otherwise,
+     * call the setActive action / mutation of the element.
+     * @param {Object} context Vuex context object.
+     * @param {Array} path Path leading up to the clicked menu element.
+     * @returns {void}
+     */
     clickedMenuElement ({commit, dispatch, getters}, path) {
         const {itemType} = getters.section(path);
 
@@ -15,6 +24,16 @@ export default {
         }
         console.error("Menu: A menu entry is missing the required value \"itemType\".");
     },
+    /**
+     * Activates the module with the given namespace.
+     * If it utilizes an action for activation, that is dispatched.
+     * Otherwise, commit the mutation.
+     * @param {Object} context Vuex context object.
+     * @param {Object} payload Object containing the payload.
+     * @param {String} payload.moduleNamespace Namespace of the module which should be activated.
+     * @param {String} payload.isActive Whether the module should be activated or deactivated.
+     * @returns {void}
+     */
     setElementActive ({commit, dispatch}, {moduleNamespace, isActive}) {
         const setActiveName = `Menu/${moduleNamespace}/setActive`;
 
