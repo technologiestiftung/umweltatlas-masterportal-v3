e1bfc8caf14a47531efbea2851192208d74413e8
mbgvhh <markus.besenfelder@gv.hamburg.de>
2022-11-04 13:18:40 +0100

    update remove isMobile-getter
    Responsibility are implemented with mediaquery.

--- src_3_0_0/modules/menu/components/MenuContainerHeaderTitle.vue
+++ src_3_0_0/modules/menu/components/MenuContainerHeaderTitle.vue
@@ -1,6 +1,4 @@
 <script>
-import {mapGetters} from "vuex";
-
 export default {
     name: "MenuContainerHeaderTitle",
     props: {
@@ -29,23 +27,17 @@ export default {
             type: String,
             default: ""
         }
-    },
-    computed: {
-        ...mapGetters(["isMobile"])
     }
 };
 </script>
 
 <template>
     <a
-        v-if="!isMobile"
-        :id="'menu-offcanvas-header-title-' + idAppendix"
+        :id="'mp-menu-header-title-' + idAppendix"
         :href="link ? link : '#'"
         :target="link ? '_blank' : '_self'"
-        :data-bs-toggle="text"
-        data-bs-placement="bottom"
         :title="toolTip"
-        class="header-title"
+        class="mp-menu-header-title"
     >
 
         <img
@@ -60,21 +52,24 @@ export default {
 <style lang="scss" scoped>
 @import "~variables";
 
-.header-title {
-    display: grid;
-    grid-template-columns: 7.5em 20.5em;
-    grid-column-gap: 1em;
-    align-items: center;
-    overflow-wrap: break-word;
+@media (min-width: 768px) {
+    .mp-menu-header-title {
+        display: grid;
+        grid-template-columns: 7.5em 20.5em;
+        grid-column-gap: 1em;
+        align-items: center;
+        overflow-wrap: break-word;
 
-    img {
-        max-height: 40px;
-    }
+        img {
+            max-height: 40px;
+        }
 
-    h1 {
-        color: $secondary_contrast;
-        font-size: 26px;
-        font-family: $font_family_narrow;
+        h1 {
+            color: $secondary_contrast;
+            font-size: 26px;
+            font-family: $font_family_narrow;
+        }
     }
 }
+
 </style>
