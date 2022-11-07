c5b2d5cab01fa52eb1715a574beff5c0d798af8b
Klara_B <klara.boecker@gv.hamburg.de>
2022-11-07 10:02:42 +0100

    BG-3392 added jsdoc

--- src_3_0_0/modules/controls/orientation/store/stateOrientation.js
+++ src_3_0_0/modules/controls/orientation/store/stateOrientation.js
@@ -7,6 +7,14 @@
  * @property {String[]} supportedDevice Devices on which the module is displayed.
  * @property {String[]} supportedMapMode Map mode in which this module can be used.
  * @property {String} zoomMode The zoomMode in config.json.
+ * @property {String} activeCategory The active tab in the overview table.
+ * @property {String} geolocation The current geolocation.
+ * @property {String} poiMode The mode for showing the nearby pois.
+ * @property {String} poiModeCurrentPositionEnabled The status if poi mode is current position.
+ * @property {String} position The current position.
+ * @property {String} showPoi Wether POI should be shown.
+ * @property {String} showPoiChoice Wether poi choice is activated.
+ * @property {String} showPoiIcon Wether to show the poi icon.
  */
 const state = {
     iconGeolocate: "geo-alt-fill",
