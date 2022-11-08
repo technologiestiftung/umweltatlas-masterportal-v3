b563b4e0c71e685e640a5b49991a5c20fe7ba54f
Vilma Jochim <vilma.jochim@gv.hamburg.de>
2022-11-08 13:58:41 +0100

    BG-3323 state properties extended

--- src_3_0_0/modules/layerPills/store/stateLayerPills.js
+++ src_3_0_0/modules/layerPills/store/stateLayerPills.js
@@ -1,5 +1,9 @@
 /**
  * User type definition
+ * @property {Boolean} active if true, LayerPills will be rendered.
+ * @property {String[]} supportedDevices devices on which the module is displayed.
+ * @property {String[]} supportedMapModes map mode in which this module can be used.
+ * @property {String} type the type of the module.
  * @property {Object} visibleSubjectDataLayer contains all visible subjectdata layers.
  * @property {Number} startIndex start index of shown layer pills from visibleSubjectDataLayer.
  * @property {Number} endIndex end index of shown layer pills from visibleSubjectDataLayer.
@@ -7,7 +11,12 @@
  * @property {Boolean} leftScrollDisabled if true, scroll through layerpills to the left is disabled.
  * @property {Boolean} rightScrollDisabled if true, scroll through layerpills to the right is disabled.
  */
+
 const state = {
+    active: false,
+    supportedDevices: ["Desktop", "Mobile", "Table"],
+    supportedMapModes: ["2D", "3D"],
+    type: "contact",
     visibleSubjectDataLayers: [],
     startIndex: 0,
     endIndex: 0,
