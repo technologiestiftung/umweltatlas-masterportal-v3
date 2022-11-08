937254f734a556c2cbe258cbda8dd8fece0d16ff
Klara_B <klara.boecker@gv.hamburg.de>
2022-11-08 17:04:49 +0100

    BG-3310 updated state

--- src_3_0_0/modules/shareView/store/stateShareView.js
+++ src_3_0_0/modules/shareView/store/stateShareView.js
@@ -3,20 +3,20 @@
  * @typedef {Object} ShareViewState
  * @property {Boolean} active if true, ShareView will rendered
  * @property {String} id id of the ShareView component
- * @property {String} currentScale scale selected in ShareView
- * @property {String} name displayed as title (config-param)
  * @property {String} icon icon next to title (config-param)
- * @property {Boolean} renderToWindow if true, tool is rendered in a window, else in sidebar (config-param)
- * @property {Boolean} resizableWindow if true, window is resizable (config-param)
- * @property {Boolean} isVisibleInMenu if true, tool is selectable in menu (config-param)
- * @property {Boolean} deactivateGFI flag if tool should deactivate gfi (config-param)
+ * @property {String} name displayed as title (config-param)
+ * @property {String[]} supportedDevices Devices on which the module is displayed.
+ * @property {String[]} supportedMapModes Map mode in which this module can be used.
+ * @property {String} type The type of the module.
  */
 const state = {
     active: false,
     id: "shareView",
-    // defaults for config.json parameters
+    icon: "bi-share",
     name: "common:menu.tools.shareView",
-    icon: "bi-share"
+    supportedDevices: ["Desktop", "Mobile", "Table"],
+    supportedMapModes: ["2D", "3D"],
+    type: "shareView"
 };
 
 export default state;
