c52bd9b3a24101c39f32622001c2f2b7b147faee
Klara_B <klara.boecker@gv.hamburg.de>
2022-11-08 17:16:18 +0100

    BG-3392 re-added a few things

---
+++ src/modules/controls/orientation/components/poi/PoiOrientation.vue
@@ -0,0 +1,47 @@
+<script>
+import {mapGetters, mapMutations, mapActions} from "vuex";
+import getters from "../../store/gettersOrientation";
+import mutations from "../../store/mutationsOrientation";
+import Icon from "ol/style/Icon";
+
+export default {
+    name: "PoiOrientation",
+    methods: {
+        ...mapMutations("controls/orientation", Object.keys(mutations)),
+        ...mapActions("Maps", ["zoomToExtent"]),
+
+        /**
+         * Getting the image path from feature
+         * @param  {ol/feature} feat Feature
+         * @return {string} imgPath the image path
+         */
+        getImgPath (feat) {
+            let imagePath = "";
+            const style = Radio.request("StyleList", "returnModelById", feat.styleId);
+
+            if (style) {
+                const featureStyle = style.createStyle(feat, false);
+
+                if (featureStyle?.getImage?.() instanceof Icon) {
+                    imagePath = featureStyle.getImage()?.getSrc() ? featureStyle.getImage()?.getSrc() : "";
+                }
+                else {
+                    style.getLegendInfos().forEach(legendInfo => {
+                        if (legendInfo.geometryType === "Point" && legendInfo.styleObject.get("type") === "circle") {
+                            imagePath = this.createCircleSVG(legendInfo.styleObject);
+                        }
+                        else if (legendInfo.geometryType === "LineString") {
+                            imagePath = this.createLineSVG(legendInfo.styleObject);
+                        }
+                        else if (legendInfo.geometryType === "Polygon") {
+                            imagePath = this.createPolygonGraphic(legendInfo.styleObject);
+                        }
+                    });
+                }
+            }
+
+            return imagePath;
+        }
+    }
+};
+</script>
--- src_3_0_0/modules/controls/orientation/components/poi/PoiOrientation.vue
+++ src_3_0_0/modules/controls/orientation/components/poi/PoiOrientation.vue
@@ -149,39 +149,6 @@ export default {
         },
 
         /**
-         * Getting the image path from feature
-         * @param  {ol/feature} feat Feature
-         * @return {string} imgPath the image path
-         */
-        getImgPath (feat) {
-            let imagePath = "";
-            const style = Radio.request("StyleList", "returnModelById", feat.styleId);
-
-            if (style) {
-                const featureStyle = style.createStyle(feat, false);
-
-                if (featureStyle?.getImage?.() instanceof Icon) {
-                    imagePath = featureStyle.getImage()?.getSrc() ? featureStyle.getImage()?.getSrc() : "";
-                }
-                else {
-                    style.getLegendInfos().forEach(legendInfo => {
-                        if (legendInfo.geometryType === "Point" && legendInfo.styleObject.get("type") === "circle") {
-                            imagePath = this.createCircleSVG(legendInfo.styleObject);
-                        }
-                        else if (legendInfo.geometryType === "LineString") {
-                            imagePath = this.createLineSVG(legendInfo.styleObject);
-                        }
-                        else if (legendInfo.geometryType === "Polygon") {
-                            imagePath = this.createPolygonGraphic(legendInfo.styleObject);
-                        }
-                    });
-                }
-            }
-
-            return imagePath;
-        },
-
-        /**
          * Zooming to the feature point
          * @param {Event} evt click event
          * @returns {void}
