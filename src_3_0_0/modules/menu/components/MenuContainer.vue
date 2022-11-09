a74d739d3bd78a37574de024d137633bb3bea70f
mbgvhh <markus.besenfelder@gv.hamburg.de>
2022-11-09 08:36:48 +0100

    update code-cleanup

--- src_3_0_0/modules/menu/components/MenuContainer.vue
+++ src_3_0_0/modules/menu/components/MenuContainer.vue
@@ -24,12 +24,6 @@ export default {
         ...mapGetters(["uiStyle"]),
         ...mapGetters("Menu", ["mainMenuExpanded", "secondaryMenuExpanded", "titleBySide"]),
         /**
-         * @returns {boolean} Whether the menu should be initially open.
-         */
-        initiallyOpen () {
-            return this.side === "mainMenu" ? this.mainInitiallyOpen : this.secondaryInitiallyOpen;
-        },
-        /**
          * @returns {string} Defines whether the ResizeHandle should be displayed on the right or left side depending on the menu this component is rendered in.
          */
         handlePosition () {
@@ -55,15 +49,15 @@ export default {
 
 <template>
     <div
-        v-if="uiStyle !== 'SIMPLE'"
         :id="'mp-menu-' + side"
         class="mp-menu collapse"
-        :class="{
-            'mp-menu-main': side === 'mainMenu',
-            'mp-menu-secondary': side === 'secondaryMenu',
-            'mp-menu-table': uiStyle === 'TABLE',
-            'show': mainMenuExpanded && side === 'mainMenu' || secondaryMenuExpanded && side === 'secondaryMenu'
-        }"
+        :class="[
+            'mp-' + side,
+            {
+                'mp-menu-table': uiStyle === 'TABLE',
+                'show': mainMenuExpanded && side === 'mainMenu' || secondaryMenuExpanded && side === 'secondaryMenu'
+            }
+        ]"
         tabindex="-1"
         :aria-label="titleBySide(side) ? titleBySide(side).text : false"
     >
@@ -74,8 +68,8 @@ export default {
             :side="side"
         />
         <ResizeHandle
-            :id="'menu-offcanvas-resize-handle-' + side"
-            class="menu-container-handle"
+            :id="'mp-resize-handle-' + side"
+            class="mp-menu-container-handle"
             :handle-position="handlePosition"
             :min-width="0.1"
             :max-width="0.5"
@@ -86,24 +80,25 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+@import "~variables";
 .mp-menu {
     height: 100%;
-    width: 100%;
+    min-width: 100%;
     position: fixed;
-    background-color: white;
-    z-index: 1;
+    background-color: $white;
+    z-index: 2;
 }
 
-.mp-menu-main {
+.mp-mainMenu {
     left: 0px;
 }
 
-.mp-menu-secondary {
+.mp-secondaryMenu {
     top: 80%;
     position: absolute;
 }
 
-.menu-container-handle {
+.mp-menu-container-handle {
     display: none;
 }
 
@@ -111,30 +106,30 @@ export default {
 @media (min-width: 768px) {
     .mp-menu {
         top: 0px;
-        width: 400px;
+        min-width: 20%;
         position: relative;
         flex-grow: 0;
         flex-shrink: 0;
     }
 
-    .mp-menu-main {
+    .mp-mainMenu {
         left: 0px;
     }
 
-    .mp-menu-secondary {
+    .mp-secondaryMenu {
        right:0em;
        position: relative;
     }
 
-    .menu-container-handle {
+    .mp-menu-container-handle {
         display: flex;
         width: 6px;
         align-items: center;
         justify-content: center;
         top: 0em;
+        font-size: 2em;
     }
  }
-
 
 .mp-menu-table {
     height: 200px;
