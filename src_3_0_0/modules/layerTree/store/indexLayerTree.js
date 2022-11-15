c3758f25d58bca066874036c29ed704342bced02
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-11-15 16:24:30 +0100

    removed unused files
    Bg-3345

--- src_3_0_0/modules/layerTree/store/indexLayerTree.js
+++
@@ -1,8 +0,0 @@
-import mutations from "./mutationsLayerTree";
-import state from "./stateLayerTree";
-
-export default {
-    namespaced: true,
-    mutations,
-    state: {...state}
-};
--- src_3_0_0/modules/layerTree/store/mutationsLayerTree.js
+++
@@ -1,6 +0,0 @@
-import stateLayerTree from "./stateLayerTree";
-import {generateSimpleMutations} from "../../../shared/js/utils/generators";
-
-export default {
-    ...generateSimpleMutations(stateLayerTree)
-};
