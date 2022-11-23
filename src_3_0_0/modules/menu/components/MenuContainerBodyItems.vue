d4ba23b5388ae4b34446e687e7e1ec73f122d8aa
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-11-23 17:08:49 +0100

    cleaned
    BG-3511

--- src_3_0_0/modules/menu/components/MenuContainerBodyItems.vue
+++ src_3_0_0/modules/menu/components/MenuContainerBodyItems.vue
@@ -40,7 +40,6 @@ export default {
                     properties = stateProperties;
                 }
             }
-            // console.log("### properties:", properties);
 
             return properties;
         }
