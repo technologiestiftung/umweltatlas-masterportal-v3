fe085c4cb0faad0ead498c9af33172bdf1754d9d
Klara_B <klara.boecker@gv.hamburg.de>
2022-11-29 13:55:05 +0100

    BG-3491 changes after rebase

--- src_3_0_0/modules/print/components/PrintMap.vue
+++ src_3_0_0/modules/print/components/PrintMap.vue
@@ -510,25 +510,22 @@ export default {
             </div>
             <div
                 v-if="isGfiAvailable"
-                class="form-group form-group-sm row"
             >
-                <label
-                    class="col-md-5 col-form-label pt-0"
-                    for="printGfi"
-                >
-                    {{ $t("common:modules.tools.print.withInfoLabel") }}
-                </label>
-                <div class="col-md-7">
-                    <div class="form-check">
-                        <input
-                            id="printGfi"
-                            type="checkbox"
-                            class="form-check-input"
-                            :disabled="currentFeature === null"
-                            :checked="isGfiSelected"
-                            @change="setIsGfiSelected($event.target.checked)"
-                        >
-                    </div>
+                <div class="form-check">
+                    <input
+                        id="printGfi"
+                        type="checkbox"
+                        class="form-check-input"
+                        :disabled="currentFeature === null"
+                        :checked="isGfiSelected"
+                        @change="setIsGfiSelected($event.target.checked)"
+                    >
+                    <label
+                        class="col-md-5 col-form-label pt-0"
+                        for="printGfi"
+                    >
+                        {{ $t("common:modules.tools.print.withInfoLabel") }}
+                    </label>
                 </div>
             </div>
             <div class="form-group form-group-sm row">
