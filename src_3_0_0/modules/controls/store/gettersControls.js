5d0b9e0610cc8a0e08ae6434ff1a29b8e680dd33
Klara_B <klara.boecker@gv.hamburg.de>
2022-12-06 13:12:12 +0100

    BG-3542 rebase fixes

--- src_3_0_0/modules/controls/store/gettersControls.js
+++
@@ -1,8 +0,0 @@
-import {generateSimpleGetters} from "../../../shared/js/utils/generators";
-import stateControls from "./stateControls";
-
-const getters = {
-    ...generateSimpleGetters(stateControls)
-};
-
-export default getters;
