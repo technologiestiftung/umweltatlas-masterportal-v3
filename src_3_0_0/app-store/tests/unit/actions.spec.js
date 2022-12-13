b61360f0631486cc0eb539172f06cdba1b18de4d
Inka Dudek <inka.dudek@gv.hamburg.de>
2022-12-13 15:02:56 +0100

    configuration of 'Fachdaten' changed in config.json
    BG-3597

--- src_3_0_0/app-store/tests/unit/actions.spec.js
+++ src_3_0_0/app-store/tests/unit/actions.spec.js
@@ -13,6 +13,182 @@ describe("src_3_0_0/app-store/actions.js", () => {
         layerConf = "./services.json";
 
     beforeEach(() => {
+        layerList = [
+            {
+                id: "453",
+                name: "name453",
+                typ: "WMS",
+                type: "layer",
+                datasets: [{
+                    md_id: "B6A59A2B-2D40-4676-9094-0EB73039ED34",
+                    md_name: "md_name_453"
+                }
+                ]
+            },
+            {
+                id: "452",
+                name: "name452",
+                typ: "WMS",
+                type: "layer",
+                datasets: [{
+                    md_id: "B6A59A2B-2D40-4676-9094-efg",
+                    md_name: "md_name_452"
+                }
+                ]
+            },
+            {
+                id: "1132",
+                name: "name1132",
+                typ: "SENSORTHINGS",
+                type: "layer",
+                datasets: [{
+                    md_id: "B6A59A2B-2D40-4676-9094-abc",
+                    md_name: "md_name_1132"
+                }
+                ]
+            },
+            {
+                id: "10220",
+                name: "layer10220",
+                typ: "WFS",
+                type: "layer",
+                datasets: [{
+                    md_id: "B6A59A2B-2D40-4676-9094-hghghg",
+                    md_name: "md_name_10220"
+                }
+                ]
+            },
+            {
+                id: "451",
+                name: "name451",
+                typ: "WFS",
+                type: "layer"
+            },
+            {
+                id: "1103",
+                name: "Überschwemmungsgebiete",
+                typ: "WMS",
+                transparent: true,
+                transparency: 0,
+                datasets: [{
+                    md_id: "0879B86F-4F44-45AA-BA5B-021D9D30AAEF"
+                }]
+            },
+            {
+                id: "717",
+                name: "name717",
+                layers: "layer717",
+                maxScale: "10000",
+                minScale: "10",
+                typ: "WMS"
+            },
+            {
+                id: "718",
+                name: "name718",
+                layers: "layer718",
+                maxScale: "30000",
+                minScale: "30",
+                typ: "WMS"
+            },
+            {
+                id: "719",
+                name: "name719",
+                layers: "layer719",
+                maxScale: "20000",
+                minScale: "20",
+                typ: "WMS"
+            }
+        ];
+        layerConfig = {
+            Hintergrundkarten: {
+                elements: [
+                    {
+                        id: "453",
+                        type: "layer",
+                        visibility: true
+                    },
+                    {
+                        id: "452",
+                        type: "layer"
+                    }
+                ]
+            },
+            Fachdaten: {
+                elements: [
+                    {
+                        id: "1132",
+                        name: "100 Jahre Stadtgruen POIs",
+                        type: "layer",
+                        visibility: true
+                    },
+                    {
+                        id: "10220",
+                        type: "layer"
+                    }
+                ]
+            }
+        };
+        layerConfigCustom = {
+            Hintergrundkarten: {
+                elements: [
+                    {
+                        id: [
+                            "717",
+                            "718",
+                            "719"
+                        ],
+                        visibility: true,
+                        type: "layer",
+                        name: "Geobasiskarten (farbig)"
+                    },
+                    {
+                        id: "453",
+                        type: "layer"
+                    }
+                ]
+            },
+            Fachdaten: {
+                elements: [
+                    {
+                        name: "Lage und Gebietszugehörigkeit",
+                        type: "folder",
+                        elements: [
+                            {
+                                name: "Überschwemmungsgebiete",
+                                type: "folder",
+                                elements: [
+                                    {
+                                        name: "Überschwemmungsgebiete",
+                                        type: "folder",
+                                        elements: [
+                                            {
+                                                id: "1103",
+                                                type: "layer"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        id: "10220",
+                                        type: "layer"
+                                    }
+                                ]
+                            },
+                            {
+                                id: "10220"
+                            },
+                            {
+                                id: "10220",
+                                type: "layer"
+                            },
+                            {
+                                id: "451",
+                                type: "layer"
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
         commit = sinon.spy();
         state = {
             configJs: {
@@ -65,6 +241,159 @@ describe("src_3_0_0/app-store/actions.js", () => {
 
             expect(initializeLayerListSpy.calledOnce).to.be.true;
             expect(initializeLayerListSpy.calledWith(layerConf)).to.be.true;
+        });
+        it("extendLayers for simple tree", () => {
+            state.layerConfig = layerConfig;
+            actions.extendLayers({commit, state});
+            expect(commit.callCount).to.be.equals(4);
+            expect(commit.alwaysCalledWith("replaceByIdInLayerConfig"));
+            expect(commit.firstCall.args[1]).to.deep.equals({layerConfigs: [{layer: Object.assign({...layerList[0]}, {visibility: true}), id: layerList[0].id}]});
+            expect(commit.secondCall.args[1]).to.deep.equals({layerConfigs: [{layer: {...layerList[1]}, id: layerList[1].id}]});
+            expect(commit.thirdCall.args[1]).to.deep.equals({layerConfigs: [{layer: Object.assign({...layerList[2]}, {visibility: true}), id: layerList[2].id}]});
+            expect(commit.lastCall.args[1]).to.deep.equals({layerConfigs: [{layer: {...layerList[3]}, id: layerList[3].id}]});
+        });
+        it("extendLayers for custom tree", () => {
+            const mergedLayer = {
+                id: "717",
+                layers: "layer717,layer718,layer719",
+                visibility: true,
+                name: "Geobasiskarten (farbig)",
+                maxScale: 30000,
+                minScale: 10,
+                showInLayerTree: true,
+                typ: "WMS",
+                type: "layer"
+            };
+
+            state.layerConfig = layerConfigCustom;
+            actions.extendLayers({commit, state});
+            expect(commit.callCount).to.be.equals(7);
+            expect(commit.alwaysCalledWith("replaceByIdInLayerConfig"));
+            expect(commit.firstCall.args[1].layerConfigs[0].layer).to.deep.equals(mergedLayer);
+            expect(commit.secondCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[0]);
+            expect(commit.thirdCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[5]);
+            expect(commit.lastCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[4]);
+        });
+        it("extendLayers for auto tree with all filtered raw layers", () => {
+            const getters = {activeCategory: {
+                "key": "kategorie_opendata",
+                "name": "Opendata",
+                "active": true
+            }};
+            let expectedFirstCallArg = null;
+
+            state.portalConfig = {tree: {
+                type: "auto",
+                validLayerTypesAutoTree: ["WMS", "SENSORTHINGS", "TERRAIN3D", "TILESET3D", "OBLIQUE"]
+            }};
+            state.layerConfig = layerConfig;
+            delete state.layerConfig.Fachdaten;
+
+            actions.extendLayers({commit, getters, state});
+
+            layerList.splice(3, 2);
+            layerList.splice(4, 3);
+            expectedFirstCallArg = {layerConfigs: layerList, parentKey: "Fachdaten"};
+
+            expect(commit.callCount).to.be.equals(3);
+            expect(commit.firstCall.args[0]).to.equals("setLayerConfigByParentKey");
+            expect(commit.firstCall.args[1]).to.deep.equals(expectedFirstCallArg);
+            expect(commit.secondCall.args[0]).to.equals("replaceByIdInLayerConfig");
+            expect(commit.secondCall.args[1].layerConfigs[0].layer).to.deep.equals(Object.assign({...layerList[0]}, {visibility: true}));
+            expect(commit.thirdCall.args[0]).to.equals("replaceByIdInLayerConfig");
+            expect(commit.thirdCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[1]);
+
+        });
+
+        it("extendLayers for special configuration with folders", () => {
+            layerConfig = {
+                Fachdaten: {
+                    elements: [
+                        {
+                            id: "1132",
+                            name: "100 Jahre Stadtgruen POIs",
+                            type: "layer",
+                            visibility: true
+                        },
+                        {
+                            id: "10220",
+                            type: "layer"
+                        },
+                        {
+                            name: "Titel",
+                            type: "folder",
+                            elements: [
+                                {
+                                    name: "3 Layer",
+                                    type: "folder",
+                                    elements: [
+                                        {
+                                            id: "717",
+                                            type: "layer",
+                                            visibility: true
+                                        },
+                                        {
+                                            id: "718",
+                                            type: "layer",
+                                            visibility: true
+                                        },
+                                        {
+                                            id: "719"
+                                        },
+                                        {
+                                            name: "Überschwemmungsgebiete",
+                                            type: "folder",
+                                            elements: [
+                                                {
+                                                    id: "1103",
+                                                    visibility: true,
+                                                    type: "layer"
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            };
+            state.layerConfig = layerConfig;
+
+            actions.extendLayers({commit, state});
+            expect(commit.callCount).to.be.equals(6);
+            expect(commit.alwaysCalledWith("replaceByIdInLayerConfig"));
+            expect(commit.firstCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[2]);
+            expect(commit.secondCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[3]);
+            expect(commit.thirdCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[6]);
+            expect(commit.lastCall.args[1].layerConfigs[0].layer).to.deep.equals(layerList[5]);
+        });
+
+        it("addLayerToLayerConfig", () => {
+            layerConfig = {
+                Fachdaten: {
+                    elements: []
+                }
+            };
+            state.layerConfig = layerConfig;
+            const layerToAdd = {
+                id: "I_m_the_id",
+                name: "Trees in Hamburg",
+                typ: "WMS",
+                layers: "trees",
+                url: "https://geodienste.hamburg.de/trees",
+                version: "1.4.3",
+                visibility: true,
+                type: "layer",
+                showInLayerTree: true,
+                maxScale: 2000,
+                minScale: 12
+
+            };
+
+            actions.addLayerToLayerConfig({state}, {layerConfig: layerToAdd, parentKey: "Fachdaten"});
+            expect(state.layerConfig?.Fachdaten?.elements.length).to.equal(1);
+            expect(state.layerConfig?.Fachdaten?.elements[0]).to.deep.equal(layerToAdd);
         });
     });
 });
