3899d802d21211353d2f23b35e77840e21159dd0
mbgvhh <markus.besenfelder@gv.hamburg.de>
2022-12-30 08:55:24 +0100

    update include navigation and clean-up

--- src_3_0_0/modules/menu/menu-store/mutationsMenu.js
+++ src_3_0_0/modules/menu/menu-store/mutationsMenu.js
@@ -6,16 +6,6 @@ export default {
     ...generateSimpleMutations(menuState),
 
     /**
-     * Adds a module state to a menu side section.
-     * @param {Object} state the state
-     * @param {Object} module Type of a module.
-     * @returns {void}
-     */
-    addModuleToMenuSection (state, {side, module}) {
-        state[side].sections[0].push(module);
-    },
-
-    /**
      * Toggles Menucontainers
      * @param {Object} currentState current state
      * @param {String} side secondary or main Menu
@@ -46,26 +36,19 @@ export default {
         currentState.secondaryMenu.expanded = false;
     },
 
+
     /**
-     * Sets current shown Component
-     * @param {Object} currentState current state
+     * Sets currently shown Component
+     * @param {Object} state current state
      * @param {String} side secondary or main Menu
      * @param {String} component Type of Component
      * @returns {void}
      */
-    setCurrentComponent (currentState, {side, component}) {
-        currentState[side].currentComponent = component;
-    },
-    /**
-     * Adds a path for a menu entry to the entries.
-     * @param {Object} state Local vuex state.
-     * @param {Array} path Path to a menu entry.
-     * @returns {void}
-     */
-    addEntry (state, {component, side}) {
-
-        state[side].navigation.history.push(state[side].navigation.currentComponent);
-        state[side].navigation.currentComponent = component;
+    setCurrentComponent (state, {component, side}) {
+        if (state[side].navigation.currentComponent !== component) {
+            state[side].navigation.history.push(state[side].navigation.currentComponent);
+            state[side].navigation.currentComponent = component;
+        }
     },
     /**
      * Removes the last path of an entry from the given side.
@@ -73,18 +56,8 @@ export default {
      * @param {String} side Side on which the last entry should be removed.
      * @returns {void}
      */
-    switchToLastEntry (state, side) {
-        state[side].navigation.currentComponent = state[side].navigation.history[0];
+    switchToPreviousComponent (state, side) {
+        state[side].navigation.currentComponent = state[side].navigation.history.slice(-1).toString();
         state[side].navigation.history.pop();
-    },
-
-    /**
-     * Reset the one menu entry.
-     * @param {Object} state Local vuex state.
-     * @param {String} side The side to reset.
-     * @returns {void}
-     */
-    resetEntry (state, side) {
-        state[side].navigation.history = [];
     }
 };
