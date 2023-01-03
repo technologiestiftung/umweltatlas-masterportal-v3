3b9f2bd14f0c56e54634e490b34c518ab836ef81
Klara_B <klara.boecker@gv.hamburg.de>
2023-01-03 11:24:52 +0100

    BG-3352 fixed test

--- src_3_0_0/modules/layerTree/layerSelection/tests/unit/components/LayerSelectionTreeNode.spec.js
+++ src_3_0_0/modules/layerTree/layerSelection/tests/unit/components/LayerSelectionTreeNode.spec.js
@@ -11,8 +11,7 @@ describe("src_3_0_0/modules/layerTree/layerSelection/components/LayerSelectionTr
     let store,
         wrapper,
         layer,
-        propsData,
-        clickSpy;
+        propsData;
 
     beforeEach(() => {
         layer = {
@@ -28,7 +27,6 @@ describe("src_3_0_0/modules/layerTree/layerSelection/components/LayerSelectionTr
         };
         store = createStore({
         });
-        clickSpy = sinon.spy(LayerSelectionTreeNode.methods, "folderClicked");
     });
 
     afterEach(() => {
@@ -63,38 +61,7 @@ describe("src_3_0_0/modules/layerTree/layerSelection/components/LayerSelectionTr
         });
 
         expect(wrapper.find(".no-list").exists()).to.be.true;
-        expect(wrapper.findAll("button").length).to.be.equals(1);
-        expect(wrapper.findAll("span").length).to.be.equals(2);
-        expect(wrapper.findAll("span")[0].text()).to.be.equals("Titel Ebene 1");
-        expect(wrapper.findAll("span")[1].text()).to.be.equals("description");
         expect(wrapper.findAll("layer-stub").length).to.be.equals(0);
     });
-    it("click on a folder shall call method", async () => {
-        let btn = null;
-
-        propsData = {
-            conf: {
-                name: "Titel Ebene 1",
-                description: "description",
-                type: "folder",
-                elements: []
-            }
-        };
-        wrapper = shallowMount(LayerSelectionTreeNode, {
-            global: {
-                plugins: [store]
-            },
-            propsData
-        });
-
-        expect(wrapper.find(".no-list").exists()).to.be.true;
-        expect(wrapper.findAll("button").length).to.be.equals(1);
-        btn = wrapper.find("button");
-
-        btn.trigger("click");
-        await wrapper.vm.$nextTick();
-        expect(clickSpy.calledOnce).to.be.true;
-    });
-
 
 });
