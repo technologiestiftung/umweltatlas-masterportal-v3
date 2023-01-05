4654f7359bc442e9cbe9283be70dbd0798e888fa
Klara_B <klara.boecker@gv.hamburg.de>
2023-01-05 12:51:02 +0100

    BG-3611 first test

--- src_3_0_0/modules/bufferAnalysis/tests/unit/components/BufferAnalysis.spec.js
+++ src_3_0_0/modules/bufferAnalysis/tests/unit/components/BufferAnalysis.spec.js
@@ -10,7 +10,24 @@ import FakeTimers from "@sinonjs/fake-timers";
 config.global.mocks.$t = key => key;
 
 describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () => {
-    let store, originalCheckIntersection, originalShowBuffer, originalApplyValuesFromSavedUrlBuffer, wrapper;
+    let store,
+        checkIntersectionSpy,
+        showBufferSpy,
+        applyValuesFromSavedUrlBufferSpy,
+        loadSelectOptionsSpy,
+        wrapper;
+
+    const mockConfigJson = {
+        Portalconfig: {
+            navigationSecondary: {
+                sections: [
+                    {
+                        "type": "bufferAnalysis"
+                    }
+                ]
+            }
+        }
+    };
 
     before(() => {
         mapCollection.clear();
@@ -24,12 +41,14 @@ describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () =>
     });
 
     beforeEach(() => {
-        originalCheckIntersection = BufferAnalysis.actions.checkIntersection;
-        originalShowBuffer = BufferAnalysis.actions.showBuffer;
-        originalApplyValuesFromSavedUrlBuffer = BufferAnalysis.actions.applyValuesFromSavedUrlBuffer;
-        BufferAnalysis.actions.checkIntersection = sinon.spy();
-        BufferAnalysis.actions.showBuffer = sinon.spy();
-        BufferAnalysis.actions.applyValuesFromSavedUrlBuffer = sinon.spy();
+        checkIntersectionSpy = sinon.spy();
+        showBufferSpy = sinon.spy();
+        applyValuesFromSavedUrlBufferSpy = sinon.spy();
+        loadSelectOptionsSpy = sinon.spy();
+        // BufferAnalysis.actions.checkIntersection = sinon.spy();
+        // BufferAnalysis.actions.showBuffer = sinon.spy();
+        // BufferAnalysis.actions.applyValuesFromSavedUrlBuffer = sinon.spy();
+        // BufferAnalysis.actions.loadSelectOptions = sinon.spy();
 
         store = createStore({
             namespaces: true,
@@ -37,13 +56,32 @@ describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () =>
                 Modules: {
                     namespaced: true,
                     modules: {
-                        BufferAnalysis
+                        namespaced: true,
+                        BufferAnalysis: {
+                            namespaced: true,
+                            actions: {
+                                checkIntersection: checkIntersectionSpy,
+                                showBuffer: showBufferSpy,
+                                applyValuesFromSavedUrlBuffer: applyValuesFromSavedUrlBufferSpy,
+                                loadSelectOptions: loadSelectOptionsSpy
+                            },
+                            mutations: {
+                                setActive: sinon.spy()
+                            },
+                            getters: {
+                                active: () => sinon.stub(),
+                                selectOptions: () => sinon.stub()
+                            }
+                        }
                     }
                 },
                 Maps: {
                     namespaced: true,
                     mode: "2D"
                 }
+            },
+            state: {
+                configJson: mockConfigJson
             }
         });
         store.commit("Modules/BufferAnalysis/setActive", true);
@@ -59,16 +97,15 @@ describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () =>
 
         mapCollection.clear();
         mapCollection.addMap(map, "2D");
-        BufferAnalysis.actions.checkIntersection = originalCheckIntersection;
-        BufferAnalysis.actions.showBuffer = originalShowBuffer;
-        BufferAnalysis.actions.applyValuesFromSavedUrlBuffer = originalApplyValuesFromSavedUrlBuffer;
-        store.commit("Modules/BufferAnalysis/setActive", false);
-        store.commit("Modules/BufferAnalysis/setSelectOptions", []);
-        store.commit("Modules/BufferAnalysis/setBufferRadius", 0);
-        store.dispatch("Modules/BufferAnalysis/resetModule");
-        if (wrapper) {
-            wrapper.destroy();
-        }
+        // BufferAnalysis.actions.checkIntersection = originalCheckIntersection;
+        // BufferAnalysis.actions.showBuffer = originalShowBuffer;
+        // BufferAnalysis.actions.applyValuesFromSavedUrlBuffer = originalApplyValuesFromSavedUrlBuffer;
+        // BufferAnalysis.actions.loadSelectOptions = originalLoadSelectOptions;
+        // store.commit("Modules/BufferAnalysis/setActive", false);
+        // store.commit("Modules/BufferAnalysis/setSelectOptions", []);
+        // store.commit("Modules/BufferAnalysis/setBufferRadius", 0);
+        // store.dispatch("Modules/BufferAnalysis/resetModule");
+        sinon.restore();
     });
 
     it("renders the bufferAnalysis", () => {
@@ -78,16 +115,7 @@ describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () =>
             }});
 
         expect(wrapper.find("#tool-bufferAnalysis").exists()).to.be.true;
-    });
-
-    it("do not render the bufferAnalysiss select if not active", () => {
-        store.commit("Modules/BufferAnalysis/setActive", false);
-        wrapper = shallowMount(BufferAnalysisComponent, {
-            global: {
-                plugins: [store]
-            }});
-
-        expect(wrapper.find("#tool-bufferAnalysis").exists()).to.be.false;
+        expect(wrapper.find("#tool-bufferAnalysis").exists()).to.be.true;
     });
 
     it("has initially set nothing to layer-analysis-select-source and layer-analysis-select-target", () => {
@@ -114,7 +142,7 @@ describe("src_3_0_0/modules/bufferAnalysis/components/BufferAnalysis.vue", () =>
         await wrapper.vm.$nextTick();
 
         options = wrapper.findAll("option");
-        expect(options.length).to.equals(8); // 2 * 3 (selectOptions) + 2 (resultType)
+        expect(options.length).to.equals(2);
     });
 
     it("triggers showBuffer action when source layer and buffer radius are set", async () => {
