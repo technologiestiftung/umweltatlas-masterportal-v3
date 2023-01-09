3170fe765408d76edfcd9daca516f6e9fbd09560
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-01-09 16:37:55 +0100

    BG-3616: update jsdoc state

--- src_3_0_0/modules/language/store/stateLanguage.js
+++ src_3_0_0/modules/language/store/stateLanguage.js
@@ -2,6 +2,12 @@
  * User type definition
  * @typedef {Object} languageState
  * @property {String} currentLocale - the current language code
+ * @property {String} type type of the module
+ * @property {Boolean} active if true, language module will rendered
+ * @property {String} description The description that should be shown
+ * @property {Boolean} showDescription If true, description will be shown.
+ * @property {String} icon icon of the module
+ * @property {String} name name of the module
  *
  */
 const state = {
@@ -9,6 +15,7 @@ const state = {
     type: "language",
     active: false,
     description: "",
+    showDescription: false,
     icon: "bi-flag",
     name: "common:menu.tools.language"
 };
