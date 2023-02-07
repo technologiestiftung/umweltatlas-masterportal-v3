39288a6f23b833fad4507614019c03d3b2bc6349
a3rtgm <info@artagam.de>
2023-02-07 08:15:44 +0100

    fix casing error dayjs

--- src/utils/detectIso8601Precision.js
+++ src/utils/detectIso8601Precision.js
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import customParseFormat from "dayjs/plugin/CustomParseFormat";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.extend(customParseFormat);
 
