2034ca6af1bcb6b18fa57e0146a8ba807c9ec676
Jonas Weiter <jonas.weiter@muenchen.de>
2023-02-27 10:53:35 +0100

    fix linter errors

--- src/modules/controls/ControlIcon.vue
+++ src/modules/controls/ControlIcon.vue
@@ -66,8 +66,10 @@ export default {
                 stroke="currentColor"
                 stroke-width="1"
                 class="bi bi-plus-lg"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    fill-rule="evenodd"
                     d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
                 />
             </svg>
@@ -85,8 +87,10 @@ export default {
                 stroke="currentColor"
                 stroke-width="1"
                 class="bi bi-dash-lg"
-                viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
+                viewBox="0 0 16 16"
+            >
+                <path
+                    fill-rule="evenodd"
                     d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"
                 />
             </svg>
