ff19927df4d4ad2271f060b70068292fa94a3513
mbgvhh <markus.besenfelder@gv.hamburg.de>
2023-03-01 11:49:05 +0100

    update overflow layertree
    to prevent nested scrollbars and preserve usability on mobile
    devices overflow for layertree is
    removed. Await feedback and maybe reintroduce it solely in dektop-
    version

--- src_3_0_0/modules/layerTree/components/LayerTree.vue
+++ src_3_0_0/modules/layerTree/components/LayerTree.vue
@@ -79,16 +79,16 @@ export default {
 
 <style lang="scss" scoped>
 @import "~variables";
-    .layer-tree{
+    .layer-tre {
         padding-left: $padding;
         font-size: $font-size-base;
-        overflow-y: auto;
         max-height: 350px;
     }
 
-    .sticky{
+    .sticky {
         position : sticky;
         bottom:0;
     }
+
 
 </style>
