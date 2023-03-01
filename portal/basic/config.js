89d74a750c378db4530217f00614d24458f37ac7
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2023-03-01 15:22:00 +0100

    update configs of basic portal

--- portal/basic/config.js
+++ portal/basic/config.js
@@ -1,29 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 const Config = {
-    wfsImgPath: "./resources/img/",
-    namedProjections: [
-        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
-    ],
-    footer: {
-        urls: [
-            {
-                "bezeichnung": "common:modules.footer.designation",
-                "url": "https://geoinfo.hamburg.de/",
-                "alias": "Landesbetrieb Geoinformation und Vermessung",
-                "alias_mobil": "LGV"
-            }
-        ],
-        showVersion: true
-    },
-    quickHelp: {
-        imgPath: "./resources/img/"
-    },
-    layerConf: "./resources/services-internet.json",
-    restConf: "./resources/rest-services-internet.json",
-    styleConf: "./resources/style_v3.json",
-    scaleLine: true,
     alerting: {
         fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
     },
+    namedProjections: [
+        ["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"]
+    ],
+    layerConf: "./resources/services-internet.json",
+    restConf: "./resources/rest-services-internet.json",
+    styleConf: "./resources/style_v3.json",
+    wfsImgPath: "./resources/img/",
     portalLanguage: {
         enabled: true,
         debug: false,
@@ -40,14 +26,5 @@ const Config = {
         },
         fallbackLanguage: "de",
         changeLanguageOnStartWhen: ["querystring", "localStorage", "htmlTag"]
-    },
-    mouseHover: {
-        numFeaturesToShow: 2,
-        infoText: "(weitere Objekte. Bitte zoomen.)"
     }
 };
-
-// conditional export to make config readable by e2e tests
-if (typeof module !== "undefined") {
-    module.exports = Config;
-}
