9f52f9760d852f0516e0104e715b043000b46ccf
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-03-01 08:35:05 +0100

    BG-3825: add api function

--- src_3_0_0/modules/getFeatureInfo/components/GetFeatureInfo.vue
+++ src_3_0_0/modules/getFeatureInfo/components/GetFeatureInfo.vue
@@ -2,7 +2,7 @@
 import {mapGetters, mapMutations, mapActions} from "vuex";
 
 import GetFeatureInfoDetached from "./GetFeatureInfoDetached.vue";
-import {mapAttributes} from "../../../shared/js/utils/attributeMapper";
+import {mapAttributes} from "@masterportal/masterportalapi/src/lib/attributeMapper";
 import omit from "../../../shared/js/utils/omit";
 import IconButton from "../../../shared/modules/buttons/components/IconButton.vue";
 
