418f0448c3cfe7d7dfd2961f57ccc1dc2c3894ab
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-03-02 09:21:01 +0100

    BG-3809: add unit test

--- src_3_0_0/plugins/tests/unit/addons.spec.js
+++ src_3_0_0/plugins/tests/unit/addons.spec.js
@@ -1,16 +1,20 @@
 import {expect} from "chai";
-import {config} from "@vue/test-utils";
 import sinon from "sinon";
-import addons from "../../plugins/addons";
+import addons from "../../../plugins/addons";
+import main from "../../../main";
 
 
-describe.only("src/plugins/addons.js", () => {
+describe("src/plugins/addons.js", () => {
+    const globalProperties = {
+        config:
+        {
+            globalProperties:
+            {}
+        }
+    };
 
     beforeEach(() => {
-        config.globalProperties = {};
-        config.globalProperties.$toolAddons = [];
-        config.globalProperties.$gfiThemeAddons = [];
-        config.globalProperties.$controlAddons = [];
+        sinon.stub(main, "getApp").returns(globalProperties);
 
     });
 
@@ -19,10 +23,14 @@ describe.only("src/plugins/addons.js", () => {
     });
     describe("loadAddons", () => {
         it("if config is undefined nothing happens", async () => {
-            const spy = sinon.spy(addons, "loadToolAddons");
+            const spyloadToolAddons = sinon.spy(addons, "loadToolAddons"),
+                spyloadGfiThemes = sinon.spy(addons, "loadGfiThemes"),
+                loadControl = sinon.spy(addons, "loadControl");
 
             await addons.loadAddons([undefined]);
-            expect(await spy.notCalled).to.be.true;
+            expect(await spyloadToolAddons.notCalled).to.be.true;
+            expect(await spyloadGfiThemes.notCalled).to.be.true;
+            expect(await loadControl.notCalled).to.be.true;
         });
     });
 });
