07c84951cc1da54bdbded9c77ac01775a8f6b537
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-03-03 18:10:50 +0100

    added tests
    Bg-3808

--- src_3_0_0/modules/legend/tests/unit/js/legendDraw.spec.js
+++ src_3_0_0/modules/legend/tests/unit/js/legendDraw.spec.js
@@ -1,4 +1,4 @@
-import {expect} from "chai";
+import { expect } from "chai";
 import sinon from "sinon";
 import legendDraw from "../../../js/legendDraw";
 
@@ -9,9 +9,9 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
     describe("drawIntervalStyle", function () {
         it("with scalingShape 'CIRCLE_BAR'", function () {
             const style = {
-                    scalingShape: "CIRCLE_BAR",
-                    scalingAttribute: "scalingAttribute"
-                },
+                scalingShape: "CIRCLE_BAR",
+                scalingAttribute: "scalingAttribute"
+            },
                 intervalStyle = {
                     style: "x"
                 };
@@ -25,9 +25,9 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
 
         it("without scalingShape 'CIRCLE_BAR'", function () {
             const style = {
-                    scalingShape: "scalingShape",
-                    scalingAttribute: "scalingAttribute"
-                },
+                scalingShape: "scalingShape",
+                scalingAttribute: "scalingAttribute"
+            },
                 intervalStyle = {
                     style: "x"
                 };
@@ -43,10 +43,10 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
     describe("drawNominalStyle", function () {
         it("with scalingShape 'circlesegments'", function () {
             const style = {
-                    attributes: {
-                        scalingShape: "circleSegments"
-                    }
-                },
+                attributes: {
+                    scalingShape: "circleSegments"
+                }
+            },
                 nominalStyle = {
                     style: "x"
                 };
@@ -60,10 +60,10 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
 
         it("drawIntervalStyle without scalingShape 'circlesegments'", function () {
             const style = {
-                    attributes: {
-                        scalingShape: "scalingShape"
-                    }
-                },
+                attributes: {
+                    scalingShape: "scalingShape"
+                }
+            },
                 nominalStyle = {
                     style: "x"
                 };
@@ -79,20 +79,20 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
     describe("drawNominalCircleSegments", function () {
         it("simple circle segment", function () {
             const styleObject = {
-                    attributes: {
-                        scalingValues: {
-                            "a": "b",
-                            "c": "d"
-                        }
-                    },
-                    style: {
-                        getImage: () => {
-                            return {
-                                getSrc: () => "src"
-                            };
-                        }
+                attributes: {
+                    scalingValues: {
+                        "a": "b",
+                        "c": "d"
                     }
                 },
+                style: {
+                    getImage: () => {
+                        return {
+                            getSrc: () => "src"
+                        };
+                    }
+                }
+            },
                 result = legendDraw.drawNominalCircleSegments(styleObject);
 
             expect(result).to.be.an("Array");
@@ -105,30 +105,30 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
 
         it("nominal circle segment", function () {
             const styleObject = {
-                    attributes: {
-                        imageScale: "2",
-                        scalingValues: {
-                            "a": "b",
-                            "c": "d"
-                        }
-                    },
-                    style: [{
-                        getImage: () => {
-                            return {
-                                getSrc: () => "src1",
-                                getSize: () => [1, 1]
-                            };
-                        }
-                    }, {
-                        getImage: () => {
-                            return {
-                                getSrc: () => "src2",
-                                getSize: () => [2, 2]
-                            };
-                        }
+                attributes: {
+                    imageScale: "2",
+                    scalingValues: {
+                        "a": "b",
+                        "c": "d"
                     }
-                    ]
                 },
+                style: [{
+                    getImage: () => {
+                        return {
+                            getSrc: () => "src1",
+                            getSize: () => [1, 1]
+                        };
+                    }
+                }, {
+                    getImage: () => {
+                        return {
+                            getSrc: () => "src2",
+                            getSize: () => [2, 2]
+                        };
+                    }
+                }
+                ]
+            },
                 result = legendDraw.drawNominalCircleSegments(styleObject);
 
             expect(result).to.be.an("Array");
@@ -144,8 +144,91 @@ describe("src_3_0_0/modules/legend/js/legendDraw", function () {
             expect(typeof result[1].iconSize[0]).to.be.equals("number");
             expect(typeof result[1].iconSizeDifferenz).to.be.equals("number");
         });
+    });
 
-        // todo inka: hier weiter testen...
+    describe("prepareLegendForPoint", function () {
+        it("prepareLegendForPoint type icon", function () {
+            const style = {
+                type: "icon",
+                imagePath: "imagePath",
+                imageName: "imageName"
+            };
+            let result = legendDraw.prepareLegendForPoint({}, style);
 
+            expect(result.graphic).to.be.equals("imagePathimageName");
+        });
+
+        it("prepareLegendForPoint type circle", function () {
+            const style = {
+                type: "circle"
+            };
+            let result = null;
+
+            sinon.stub(legendDraw, "drawCircleStyle").returns("graphic");
+
+            result = legendDraw.prepareLegendForPoint({}, style);
+            expect(result.graphic).to.be.equals("graphic");
+        });
+
+        it("prepareLegendForPoint type interval", function () {
+            const style = {
+                type: "interval"
+            };
+            let result = null;
+
+            sinon.stub(legendDraw, "drawIntervalStyle").returns("graphic");
+
+            result = legendDraw.prepareLegendForPoint({}, style);
+            expect(result.graphic).to.be.equals("graphic");
+        });
+
+        it("prepareLegendForPoint type nominal", function () {
+            const style = {
+                type: "nominal"
+            };
+            let result = null;
+
+            sinon.stub(legendDraw, "drawNominalStyle").returns("graphic");
+
+            result = legendDraw.prepareLegendForPoint({}, style);
+            expect(result).to.be.equals("graphic");
+        });
+    });
+
+    describe("prepare", function () {
+        it("prepare geometryType Point", function () {
+            let result = null;
+
+            sinon.stub(legendDraw, "prepareLegendForPoint").returns("LegendForPoint");
+            result = legendDraw.prepare("Point", {}, "name");
+
+            expect(result).to.be.equals("LegendForPoint");
+        });
+
+        it("prepare geometryType LineString", function () {
+            let result = null;
+
+            sinon.stub(legendDraw, "prepareLegendForLineString").returns("LegendForLineString");
+            result = legendDraw.prepare("LineString", {}, "name");
+
+            expect(result).to.be.equals("LegendForLineString");
+        });
+        it("prepare geometryType Polygon", function () {
+            let result = null;
+
+            sinon.stub(legendDraw, "prepareLegendForPolygon").returns("LegendForPolygon");
+            result = legendDraw.prepare("Polygon", {}, "name");
+
+            expect(result).to.be.equals("LegendForPolygon");
+        });
+        it("prepare geometryType Cesium", function () {
+            let result = null;
+
+            sinon.stub(legendDraw, "prepareNameForCesium").returns("NameForCesium");
+            sinon.stub(legendDraw, "prepareLegendForCesium").returns("LegendForCesium");
+            result = legendDraw.prepare("Cesium", {}, "name");
+
+            expect(result).to.be.equals("LegendForCesium");
+        });
     });
 });
