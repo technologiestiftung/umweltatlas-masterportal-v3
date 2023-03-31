458555876e1ca9f2c80f6de95452844a8e83c0b9
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-03-31 07:21:50 +0200

    removed comment
    BG-3990

--- modules/core/configLoader/preparser.js
+++ modules/core/configLoader/preparser.js
@@ -100,7 +100,7 @@ const Preparser = Backbone.Model.extend(/** @lends Preparser.prototype */{
             portalConfig: response.Portalconfig,
             baselayer: response.Themenconfig.Hintergrundkarten,
             overlayer: response.Themenconfig.Fachdaten,
-            overlayer_3d: response.Themenconfig.Fachdaten_3D, // / || response.Themenconfig.Fachdaten.Fachdaten_3D,
+            overlayer_3d: response.Themenconfig.Fachdaten_3D,
             overlayer_time: response.Themenconfig.Fachdaten_Zeit,
             treeType: response.Portalconfig?.treeType ? response.Portalconfig.treeType : "light",
             isFolderSelectable: this.parseIsFolderSelectable(Config?.tree?.isFolderSelectable),
