970885eae0805f1e7a0cf50674cb5d15c724eafb
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-04-25 16:40:50 +0200

    added test fpr fillImagePath
    BG-3828

--- src_3_0_0/modules/controls/orientation/tests/unit/components/poi/PoiOrientation.spec.js
+++ src_3_0_0/modules/controls/orientation/tests/unit/components/poi/PoiOrientation.spec.js
@@ -1,6 +1,8 @@
 import {createStore} from "vuex";
 import {config, shallowMount} from "@vue/test-utils";
 import {expect} from "chai";
+import styleList from "@masterportal/masterportalapi/src/vectorStyle/styleList.js";
+import createStyle from "@masterportal/masterportalapi/src/vectorStyle/createStyle.js";
 import PoiOrientationComponent from "../../../../components/poi/PoiOrientation.vue";
 import Feature from "ol/Feature.js";
 import sinon from "sinon";
@@ -10,7 +12,10 @@ config.global.mocks.$t = key => key;
 describe("src_3_0_0/modules/controls/orientation/components/PoiOrientation.vue", () => {
     let store,
         propsData,
-        wrapper;
+        wrapper,
+        returnLegendByStyleIdSpy,
+        styleObj,
+        featureStyleObject;
 
     beforeEach(() => {
         store = createStore({
@@ -47,11 +52,33 @@ describe("src_3_0_0/modules/controls/orientation/components/PoiOrientation.vue",
                 const feature = new Feature(),
                     featuresAll = [];
 
-                featuresAll.push(feature);
+                feature.setId("first");
+                feature.set("styleId", "styleId");
 
+                featuresAll.push(feature);
                 return featuresAll;
             }
         };
+        styleObj = {
+            styleId: "styleId",
+            rules: [],
+            getImage: () => {
+                return {
+                    getSrc: () => "src"
+                };
+            }
+        };
+        featureStyleObject = {
+            styleId: "f_styleId",
+            rules: [],
+            attributes: {
+                type: "icon"
+            }
+        };
+        sinon.stub(styleList, "returnStyleObject").returns(styleObj);
+        sinon.stub(createStyle, "createStyle").returns(styleObj);
+        sinon.stub(createStyle, "getGeometryStyle").returns(featureStyleObject);
+        returnLegendByStyleIdSpy = sinon.spy(createStyle, "returnLegendByStyleId");
 
         wrapper = shallowMount(PoiOrientationComponent,
             {
@@ -62,7 +89,7 @@ describe("src_3_0_0/modules/controls/orientation/components/PoiOrientation.vue",
             });
     });
 
-    after(() => {
+    afterEach(() => {
         sinon.restore();
     });
 
@@ -92,5 +119,30 @@ describe("src_3_0_0/modules/controls/orientation/components/PoiOrientation.vue",
             });
             expect(wrapper.vm.getFeatureTitle(feature)).to.be.an("array").to.deep.equal(["nearbyTitleText"]);
         });
+    });
+
+
+    describe("fillImagePath", function () {
+        it("should fill data 'imgPathByFeature' and return image on mount.", function () {
+            expect(Object.keys(wrapper.vm.imgPathByFeature).length).to.be.equals(1);
+            expect(Object.keys(wrapper.vm.imgPathByFeature)[0]).to.be.equals("first");
+            expect(wrapper.vm.imgPathByFeature.first).to.be.equals("src");
+            expect(returnLegendByStyleIdSpy.notCalled).to.be.true;
+        });
+
+        it("should fill second entry of data 'imgPathByFeature' and call 'returnLegendByStyleId'.", function () {
+            const feature = new Feature();
+
+            feature.setId("id");
+            feature.set("styleId", "styleId");
+            featureStyleObject.attributes = {};
+            wrapper.vm.fillImagePath(feature);
+
+            expect(Object.keys(wrapper.vm.imgPathByFeature).length).to.be.equals(2);
+            expect(Object.keys(wrapper.vm.imgPathByFeature)[1]).to.be.equals("id");
+            expect(wrapper.vm.imgPathByFeature.id).not.to.be.equals("src");
+            expect(returnLegendByStyleIdSpy.calledOnce).to.be.true;
+        });
+
     });
 });
