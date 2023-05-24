edc26f414e91c8e21ff3886f5cb8d2cd2933c86c
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-05-24 14:33:10 +0200

    fixed tests
    BG-4118

--- src/core/maps/tests/unit/store/actionsMapAttributesMapper.spec.js
+++ src/core/maps/tests/unit/store/actionsMapAttributesMapper.spec.js
@@ -40,7 +40,6 @@ describe("src/core/maps/store/actions/actionsMapAttributesMapper.js", () => {
                     }
                 };
 
-
             actions.updateClick({commit, getters, rootGetters}, obj);
             expect(commit.calledTwice).to.be.true;
             expect(commit.firstCall.args).to.deep.equal([
