710613334c91a5135e10960fdaf0054ebd6624a0
Klara_B <klara.boecker@gv.hamburg.de>
2023-06-22 14:15:32 +0200

    BG-3975 sorted state and added default values

--- src_3_0_0/modules/coordToolkit/store/stateCoordToolkit.js
+++ src_3_0_0/modules/coordToolkit/store/stateCoordToolkit.js
@@ -2,78 +2,82 @@
  * User type definition
  * @module modules/CoordToolkit/state
  *
- * @property {String} description The description that should be shown in the button in the right menu.
- * @property {Boolean} hasMouseMapInteractions If this attribute is true, then all other modules will be deactivated when this attribute is also true. Only one module with this attribute true may be open at the same time, since conflicts can arise in the card interactions.
- * @property {String} [mode="supply"] may be 'search' or 'supply'
- * @property {module:ol/interaction/Pointer} selectPointerMove contains interaction listener to map
- * @property {Object[]} projections list of available projections
- * @property {Object} mapProjection projection of the map
- * @property {Number[]} positionMapProjection position of the projection in the map
- * @property {Boolean} updatePosition if true, position is updated in tool
- * @property {Object} currentProjection the current projection
- * @property {boolean} eastingNoCoord true, if no coord in easting input field
- * @property {boolean} eastingNoMatch true, if coord in easting are not valid
- * @property {boolean} northingNoCoord true, if no coord in northing input field
- * @property {boolean} northingNoMatch true, if coord in northing are not valid
- * @property {Object} coordinatesEasting contains id and value of the easting input field
- * @property {Object} coordinatesNorthing contains id and value of the northing input field
- * @property {String} coordinatesEastingExample contains the example for easting coordinates
- * @property {String} coordinatesNorthingExample contains the example for northing coordinates
- * @property {Array} selectedCoordinates contains the selected coordinates
- * @property {String} height contains the value of the height input field
- * @property {module:ol/Layer}  heightLayer must be set in config.json to display the height. The layer to get the height from.
- * @property {String} heightLayerId id of the layer to get the height from
- * @property {String} [heightInfoFormat="application/vnd.ogc.gml"] infoFormat of the layers getFeatureRequest
- * @property {String} heightElementName element name in the response of getFeatureRequest of height layer
- * @property {String} heightValueWater value in the response of getFeatureRequest of height layer, if there is water area
- * @property {String} heightValueBuilding value in the response of getFeatureRequest of height layer, if there is building area
- * @property {Boolean} showCopyButtons if true, copy-buttons are shown
- * @property {String} delimiter delimits the copies coordinates
- * @property {String} zoomLevel used by search
- * @property {String} name displayed as title (config-param)
  * @property {String} icon icon next to title (config-param)
+ * @property {String} description The description that should be shown in the button in the right menu.
+ * @property {Boolean} [hasMouseMapInteractions="true"] If this attribute is true, then all other modules will be deactivated when this attribute is also true. Only one module with this attribute true may be open at the same time, since conflicts can arise in the card interactions.
+ * @property {String} name displayed as title (config-param)
  * @property {Boolean} showDescription If true, description will be shown.
- * @property {String} heightLayerInfo contains the info text for the height layer
+ * @property {String[]} supportedDevices Devices on which the module is displayed.
+ * @property {String[]} supportedMapModes Map mode in which this module can be used.
+ * @property {String} type the type of the component
+ *
+ * @property {Object} [coordinatesEasting="{id: "easting", value: ""}"] contains id and value of the easting input field
+ * @property {String} coordinatesEastingExample contains the example for easting coordinates
  * @property {Object} coordInfo contains the explanations for the coordinate reference systems
+ * @property {Object} [coordinatesNorthing="{id: "northing", value: ""}"] contains id and value of the northing input field
+ * @property {String} coordinatesNorthingExample contains the example for northing coordinates
+ * @property {Object} [currentProjection="{id: "http://www.opengis.net/gml/srs/epsg.xml#25832", name: "EPSG:25832", projName: "utm"}"] the current projection
+ * @property {String} [delimiter="|"] delimits the copies coordinates
+ * @property {boolean} [eastingNoCoord="false"] true, if no coord in easting input field
+ * @property {boolean} [eastingNoMatch="false"] true, if coord in easting are not valid
+ * @property {String} height contains the value of the height input field
+ * @property {String} heightElementName element name in the response of getFeatureRequest of height layer
+ * @property {String} [heightInfoFormat="application/vnd.ogc.gml"] infoFormat of the layers getFeatureRequest
+ * @property {module:ol/Layer} heightLayer must be set in config.json to display the height. The layer to get the height from.
+ * @property {String} heightLayerId id of the layer to get the height from
+ * @property {String} heightLayerInfo contains the info text for the height layer
+ * @property {String} heightValueBuilding value in the response of getFeatureRequest of height layer, if there is building area
+ * @property {String} heightValueWater value in the response of getFeatureRequest of height layer, if there is water area
+ * @property {Object} mapProjection projection of the map
+ * @property {String} [mode="supply"] may be 'search' or 'supply'
+ * @property {Boolean} [northingNoCoord="false"] true, if no coord in northing input field
+ * @property {boolean} [northingNoMatch="false"] true, if coord in northing are not valid
+ * @property {Number[]} positionMapProjection position of the projection in the map
+ * @property {Object[]} projections list of available projections
+ * @property {Array} selectedCoordinates contains the selected coordinates
+ * @property {module:ol/interaction/Pointer} selectPointerMove contains interaction listener to map
+ * @property {Boolean} [showCopyButtons="true"] if true, copy-buttons are shown
+ * @property {Boolean} [updatePosition="true"] if true, position is updated in tool
+ * @property {String} [zoomLevel="7"] used by search
  */
 const state = {
     icon: "bi-globe",
+    description: "",
     hasMouseMapInteractions: true,
     name: "common:modules.coordToolkit.name",
-    type: "coordToolkit",
-    description: "",
     showDescription: false,
     supportedDevices: ["Desktop", "Mobile", "Table"],
     supportedMapModes: ["2D", "3D"],
+    type: "coordToolkit",
 
-    mode: "supply",
-    selectPointerMove: null,
-    projections: [],
-    mapProjection: null,
-    positionMapProjection: [],
-    updatePosition: true,
-    currentProjection: {id: "http://www.opengis.net/gml/srs/epsg.xml#25832", name: "EPSG:25832", projName: "utm"},
-    eastingNoCoord: false,
-    eastingNoMatch: false,
-    northingNoCoord: false,
-    northingNoMatch: false,
     coordinatesEasting: {id: "easting", value: ""},
     coordinatesNorthing: {id: "northing", value: ""},
     coordinatesEastingExample: "",
     coordinatesNorthingExample: "",
-    selectedCoordinates: [],
-    height: "",
-    heightLayer: null,
-    zoomLevel: 7,
-    heightLayerId: null,
-    heightInfoFormat: "application/vnd.ogc.gml",
-    heightElementName: null,
-    heightValueWater: null,
-    heightValueBuilding: null,
-    showCopyButtons: true,
+    coordInfo: null,
+    currentProjection: {id: "http://www.opengis.net/gml/srs/epsg.xml#25832", name: "EPSG:25832", projName: "utm"},
     delimiter: "|",
+    eastingNoCoord: false,
+    eastingNoMatch: false,
+    height: "",
+    heightElementName: null,
+    heightInfoFormat: "application/vnd.ogc.gml",
+    heightLayer: null,
+    heightLayerId: null,
     heightLayerInfo: null,
-    coordInfo: null
+    heightValueBuilding: null,
+    heightValueWater: null,
+    mapProjection: null,
+    mode: "supply",
+    northingNoCoord: false,
+    northingNoMatch: false,
+    positionMapProjection: [],
+    projections: [],
+    showCopyButtons: true,
+    selectedCoordinates: [],
+    selectPointerMove: null,
+    updatePosition: true,
+    zoomLevel: 7
 };
 
 export default state;
