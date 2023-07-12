c172bc36516be0bec33cc7ffbf9c07666f35872f
Inka Dudek <inka.dudek@gv.hamburg.de>
2023-07-12 18:07:13 +0200

    removed moved code
    Bg-4271

--- src/core/layers/vectorTile.js
+++ src/core/layers/vectorTile.js
@@ -7,7 +7,29 @@ import store from "../../app-store";
  * @returns {void}
  */
 export default function VectorTileLayer (attrs) {
-  
+    const defaults = {
+            selectedStyleID: undefined,
+            useMpFonts: true,
+            useProxy: false
+        },
+        mapEPSG = store.getters["Maps/projection"].getCode(),
+        vtEPSG = attrs.epsg || mapEPSG;
+
+    if (mapEPSG !== vtEPSG) {
+        // console.warn(`VT Layer ${attrs.name}: Map (${mapEPSG}) and layer (${vtEPSG}) projection mismatch. View will be erroneous.`);
+        attrs.isNeverVisibleInTree = true;
+    }
+    this.createLayer(Object.assign(defaults, attrs));
+    // call the super-layer
+    Layer.call(this, Object.assign(defaults, attrs), this.layer, !attrs.isChildLayer);
+
+    // set the style only at the first selection of the layer
+    if (attrs.isSelected) {
+        this.setConfiguredLayerStyle();
+    }
+    else {
+        this.layer.once("change:visible", () => this.setConfiguredLayerStyle());
+    }
 }
 
 // Link prototypes and add prototype methods, means VTL uses all methods and properties of Layer
