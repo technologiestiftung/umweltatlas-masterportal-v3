8e39dcd1c7e01d160cc957da1ada5687ce4831d4
DargeFi <finn-rasmus.darge@dataport.de>
2023-08-03 07:48:07 +0200

    edit test

--- src/modules/tools/routing/tests/unit/utils/geosearch/routing-locationFinder-geosearch.spec.js
+++ src/modules/tools/routing/tests/unit/utils/geosearch/routing-locationFinder-geosearch.spec.js
@@ -20,28 +20,54 @@ describe("src/modules/tools/routing/utils/geosearch/routing-locationFinder-geose
     describe("should fetchRoutingLocationFinderGeosearch", () => {
         it("should process result correct", async () => {
             sinon.stub(axios, "get").returns(
-                new Promise(resolve => resolve({
+                new Promise((resolve) => resolve({
                     status: 200,
                     data: {
-                        locs: [{
-                            cx: 515643.21,
-                            cy: 5404093.73,
-                            epsg: 25832,
-                            id: 2637,
-                            name: "Parkstraße",
-                            type: "Straßenname",
-                            xmax: 515719.04,
-                            xmin: 515435.23,
-                            ymax: 5404246.82,
-                            ymin: 5404023.02
-                        }],
+                        locs: [
+                            {
+                                id: 1609,
+                                type: "Straßenname",
+                                name: "Im Alten Park",
+                                cx: 511114.73,
+                                cy: 5397800.69,
+                                xmin: 511097.28,
+                                ymin: 5397769.7,
+                                xmax: 511216.35,
+                                ymax: 5397809.38
+                            },
+                            {
+                                cx: 515643.21,
+                                cy: 5404093.73,
+                                epsg: 25832,
+                                id: 2637,
+                                name: "Parkstraße",
+                                type: "Straßenname",
+                                xmax: 515719.04,
+                                xmin: 515435.23,
+                                ymax: 5404246.82,
+                                ymin: 5404023.02
+                            }
+                        ],
                         sref: 25832
                     }
-                }))
+                })
+                )
             );
 
-            const result = await fetchRoutingLocationFinderGeosearch("testsearch"),
-                expectedResult = [new RoutingGeosearchResult([515643.21, 5404093.73], "Parkstraße", "25832")];
+            const result = await fetchRoutingLocationFinderGeosearch(
+                    "testsearch"
+                ),
+                expectedResult = [new RoutingGeosearchResult(
+                    [511114.73, 5397800.69],
+                    "Im Alten Park",
+                    "25832"
+                ),
+                new RoutingGeosearchResult(
+                    [515643.21, 5404093.73],
+                    "Parkstraße",
+                    "25832"
+                )
+                ];
 
             expect(result).deep.to.eql(expectedResult);
         });
