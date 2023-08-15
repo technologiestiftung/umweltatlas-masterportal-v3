7a100d5e9e10c8be36fba95a958cc2ee0d68f30d
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-08-15 10:49:36 +0200

    BG-4339: fix unit tests

--- src/modules/tools/routing/tests/unit/utils/geosearch/routing-bkg-geosearch.spec.js
+++ src/modules/tools/routing/tests/unit/utils/geosearch/routing-bkg-geosearch.spec.js
@@ -96,8 +96,8 @@ describe("src/modules/tools/routing/utils/geosearch/routing-bkg-geosearch.js", (
 
             const result = await fetchRoutingBkgGeosearch("testsearch"),
                 expectedResult = [
-                    new RoutingGeosearchResult([51.33264, 6.56089], "Krefeld"),
-                    new RoutingGeosearchResult([51.34567, 6.51518], "47804 Krefeld - Hüls")
+                    new RoutingGeosearchResult([6.56089, 51.33264], "Krefeld"),
+                    new RoutingGeosearchResult([6.51518, 51.34567], "47804 Krefeld - Hüls")
                 ];
 
             expect(result).deep.to.equal(expectedResult);
@@ -169,7 +169,7 @@ describe("src/modules/tools/routing/utils/geosearch/routing-bkg-geosearch.js", (
             );
 
             const result = await fetchRoutingBkgGeosearchReverse("testsearch"),
-                expectedResult = new RoutingGeosearchResult([51.33329, 6.56619], "47798 Krefeld - Cracau");
+                expectedResult = new RoutingGeosearchResult([6.56619, 51.33329], "47798 Krefeld - Cracau");
 
             expect(result).deep.to.eql(expectedResult);
         });
