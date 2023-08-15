2b1663e5f9dceb4d789f4b7b26616152762ecacb
JunkerSv <sven.junker-beyel@gv.hamburg.de>
2023-08-15 16:13:35 +0200

    update template for topicTree entries was revised
    BG-4358

--- src_3_0_0/modules/searchBar/store/actions/actionsSearchBar.js
+++ src_3_0_0/modules/searchBar/store/actions/actionsSearchBar.js
@@ -1,13 +1,11 @@
 /**
  * Contains global actions of the search bar.
  */
-import actionsSearchBarResultList from "./actionsSearchBarResultList";
 import actionsSearchBarSearchInterfaces from "./actionsSearchBarSearchInterfaces";
 import actionsSearchBarSearchResult from "./actionsSearchBarSearchResult";
 import SearchInterface from "../../searchInterfaces/searchInterface";
 
 export default {
-    ...actionsSearchBarResultList,
     ...actionsSearchBarSearchInterfaces,
     ...actionsSearchBarSearchResult,
 
