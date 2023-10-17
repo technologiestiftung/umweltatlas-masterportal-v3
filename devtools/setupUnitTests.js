109833339c99cd2ae1f2c8449524520c7d4f3e89
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-10-17 12:49:40 +0200

    fix rebase conflicts

--- devtools/setupUnitTests.js
+++ devtools/setupUnitTests.js
@@ -55,41 +55,5 @@ class MutationObserver {
         this.args = toObserve;
     }
 }
-global.MutationObserver = MutationObserver;
-
-// renderStubDefaultSlot: https://test-utils.vuejs.org/migration/#shallowmount-and-renderstubdefaultslot
-config.global.renderStubDefaultSlot = true;
-// global.SVGElement: @see https://github.com/vuejs/core/issues/3590
-global.SVGElement = window.SVGElement;
-global.CanvasPattern = canvas.CanvasPattern;
-
-/**
- * Mock for MutationObserver
- */
-class MutationObserver {
-    /**
-     * Constructor
-     * @param {Object} args the args
-     * @returns {void} void
-     */
-    constructor (args) {
-        this.args = args;
-    }
-    /**
-     * Fake function.
-     * @returns {void}
-     */
-    disconnect () {
-        this.args = null;
-    }
-    /**
-     * Fake function.
-     * @param {Object} toObserve to observe
-     * @returns {void}
-     */
-    observe (toObserve) {
-        this.args = toObserve;
-    }
-}
 // a mock for MutationObserver
 global.MutationObserver = MutationObserver;
