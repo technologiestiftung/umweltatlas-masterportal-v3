f47f38640a38fd1cf1397c9b646c851b2c832385
Mohammad Reaza Mossadegh <mohammadreza.mossadegh@gv.hamburg.de>
2023-10-30 11:59:11 +0100

    G31DEV1-1827-issue-1093-gefilterte-daten - bugfix - crs corrected

--- src/modules/tools/filter/components/SnippetDownload.vue
+++ src/modules/tools/filter/components/SnippetDownload.vue
@@ -64,7 +64,12 @@ export default {
                 Array.isArray(this.filteredItems) &&
                 this.filteredItems.length > 0) {
                 if (this.selectedFormat === "GeoJSON") {
-                    this.json = new GeoJSON().writeFeatures(this.filteredItems);
+                    const parser = new GeoJSON({
+                        dataProjection: "EPSG:4326",
+                        featureProjection: mapCollection.getMapView("2D").getProjection().getCode()
+                    });
+
+                    this.json = parser.writeFeatures(this.filteredItems);
                 }
                 this.enableFileDownload = true;
             }
