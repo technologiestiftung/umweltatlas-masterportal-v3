eaafb66660191208f94e76ce7d03d95f903c0ff1
bruehlca <carina.bruehl@dataport.de>
2023-12-01 12:18:51 +0100

    add state parameter sldVersion to jsdoc

--- src_3_0_0/modules/legend/store/stateLegend.js
+++ src_3_0_0/modules/legend/store/stateLegend.js
@@ -8,6 +8,7 @@
  * @property {Boolean}  hasMouseMapInteractions If this attribute is true, then all other modules will be deactivated when this attribute is also true. Only one module with this attribute true may be open at the same time, since conflicts can arise in the card interactions.
  * @property {String[]} supportedDevices Devices on which the module is displayed.
  * @property {String[]} supportedMapModes - Map mode in which this module can be used.
+ * @property {String} sldVersion - The Verson of Style Layer Descriptor for the GetLegendGraphic request.
  * @property {Array}    legends - Contains infos about each legend.
  * @property {Array}    waitingLegendsInfos - Contains layers waiting for load end to show in layer information.
  * @property {Object}   layerInfoLegend - to show in layer information.
