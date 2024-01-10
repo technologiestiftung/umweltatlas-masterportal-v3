1c98a8c5bfa3d4f6712f611945bd7685a81d2d25
DargeFi <finn-rasmus.darge@dataport.de>
2024-01-10 08:49:32 +0100

    add jsdoc

--- src_3_0_0/modules/login/store/gettersLogin.js
+++ src_3_0_0/modules/login/store/gettersLogin.js
@@ -1,6 +1,10 @@
 import stateLogin from "./stateLogin";
 import {generateSimpleGetters} from "../../../shared/js/utils/generators";
 
+/**
+ * The getters for the Login.
+ * @module modules/login/store/gettersLogin
+ */
 const gettersMap = {
     ...generateSimpleGetters(stateLogin)
 };
--- src_3_0_0/modules/login/store/mutationsLogin.js
+++ src_3_0_0/modules/login/store/mutationsLogin.js
@@ -1,7 +1,10 @@
 import {generateSimpleMutations} from "../../../shared/js/utils/generators";
 import initialState from "./stateLogin";
 
-
+/**
+ * The mutations for the Login.
+ * @module modules/login/store/mutationsLogin
+ */
 const mutations = {
     ...generateSimpleMutations(initialState)
 };
