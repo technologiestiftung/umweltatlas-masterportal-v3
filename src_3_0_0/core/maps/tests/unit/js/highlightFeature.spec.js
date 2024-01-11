c756ae3bbcbe03459d4b9ce1f2c199abbae0f2e6
bruehlca <carina.bruehl@dataport.de>
2024-01-11 17:26:19 +0100

    add new assertion to test for type "increase"

--- src_3_0_0/core/maps/tests/unit/js/highlightFeature.spec.js
+++ src_3_0_0/core/maps/tests/unit/js/highlightFeature.spec.js
@@ -1,107 +1,75 @@
 import {expect} from "chai";
 import sinon from "sinon";
-import {highlightFeature} from "../../../js/highlightFeature";
-import {Style} from "ol/style.js";
-// import Point from "ol/geom/Point";
+import highlightFeature from "../../../js/highlightFeature";
+import {Style, Fill, Stroke, Circle} from "ol/style.js";
 
 describe("src_3_0_0/core/maps/js/highlightFeature.js", () => {
-    describe("highlightFeature", () => {
-        const feature = {
+    describe.only("highlightFeature", () => {
+        const stroke = new Stroke({
+                color: "#3399CC",
+                width: 1.25
+            }),
+            fill = new Fill({}),
+            style = new Style({
+                image: new Circle({
+                    fill: fill,
+                    stroke: stroke,
+                    radius: 5
+                }),
+                fill: fill,
+                stroke: stroke
+            }),
+            featurePoint = {
                 id: "test",
-                getGeometry: () => sinon.spy({
+                getGeometry: () => sinon.stub({
                     getType: () => "Point",
                     getCoordinates: () => [100, 100]
                 }),
                 getProperties: () => [],
                 get: () => sinon.stub(),
-                getStyle: () => new Style()
+                getStyle: () => style,
+                setStyle: () => sinon.stub()
             },
-            highlightObject = {
+            layer = {
+                id: "id",
+                getSource: () =>{
+                    return {
+                        getFeatureById: sinon.stub.returns(featurePoint),
+                        getFeatures: sinon.stub.returns([featurePoint])
+                    };
+                },
+                getLayerById: () => sinon.stub.returns(layer)
+            },
+            highlightObjectPoint = {
                 styleId: "defaultHighlightFeaturesPoint",
                 type: "increase",
-                feature: feature
+                feature: featurePoint
             };
+
         let commit,
             dispatch,
             getters,
             increaseFeatureSpy;
 
-        before(() => {
-            increaseFeatureSpy = sinon.spy(highlightFeature, "increaseFeature");
+        beforeEach(() => {
             commit = sinon.spy();
             dispatch = sinon.spy();
+            increaseFeatureSpy = sinon.spy(highlightFeature, "increaseFeature");
+        });
 
+        afterEach(() => {
+            sinon.restore();
         });
 
         it("tests highlightFeature with type 'increase'", () => {
 
-            highlightFeature({commit, dispatch, getters}, highlightObject);
-            // TODO: should be true but is false
-            expect(increaseFeatureSpy.calledOnce).to.be.false;
-            // expect(Object.keys(olMap.listeners_)).include("pointermove");
+            highlightFeature.highlightFeature({commit, dispatch, getters}, highlightObjectPoint);
+            expect(increaseFeatureSpy.called).to.be.true;
+            expect(commit.called).to.be.true;
+            expect(commit.firstCall.args[0]).to.equals("Maps/addHighlightedFeature");
+            expect(commit.firstCall.args[1]).to.equals(featurePoint);
+            expect(commit.secondCall.args[0]).to.equals("Maps/addHighlightedFeatureStyle");
+            expect(commit.secondCall.args[1]).to.equals(style);
         });
-
-
     });
 });
-
-
-// import sinon from "sinon";
-
-// import {highlightFeature} from "../../../js/highlightFeature";
-
-// describe("src_3_0_0/core/maps/js/highlightFeature", () => {
-//     describe("highlightFeature", () => {
-//         it("should check how to highlight", function () {
-//             const highlightObject = {
-//                     type: "highlightPolygon"
-//                 },
-//                 commit = sinon.spy(),
-//                 dispatch = sinon.spy();
-
-//             highlightFeature({commit, dispatch}, highlightObject);
-//         });
-//     });
-
-/* describe("highlightPolygon", () => {
-        it("should highlight a polygon feature", function () {
-            const highlightObject = {
-                    feature: "..."
-                },
-                commit = sinon.spy(),
-                dispatch = sinon.spy();
-
-            highlightPolygon(commit, dispatch, highlightObject);
-        });
-    }); */
-/*
-    describe("highlightLine", () => {
-        it("should highlight a line feature", function () {
-
-        });
-    });
-
-    describe("highlightViaParametricUrl", () => {
-        it("should highlight a feature via layerid and featureid", function () {
-
-        });
-    });
-
-    describe("getHighlightFeature", () => {
-        it("should search the feature which shall be highlighted", function () {
-
-        });
-    });
-
-    describe("increaseFeature", () => {
-        it("should increase the icon of the feature", function () {
-
-        });
-    });
-
-    describe("styleObject", () => {
-        it("should get style via styleList", function () {
-
-        });
-    }); */
-// });
