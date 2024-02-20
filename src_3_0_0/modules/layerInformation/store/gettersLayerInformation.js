d251f79bf501f40f37c0940c5b7f2d84d6ac444d
Inka Dudek <inka.dudek@gv.hamburg.de>
2024-02-20 13:52:37 +0100

    changed urlParams
    BG-5178

--- src_3_0_0/modules/layerInformation/store/gettersLayerInformation.js
+++ src_3_0_0/modules/layerInformation/store/gettersLayerInformation.js
@@ -9,38 +9,13 @@ export default {
     ...generateSimpleGetters(stateLayerInformation),
 
     /**
-     * Provides state for urlParams, encodes uris in legends.
+     * Provides state for urlParams.
      * @param {Object} state state of the app-store.
      * @returns {Object} state for urlParams
      */
     urlParams: state => {
-        const stateCopy = {...state},
-            downloadLinks = [];
-
-        Object.entries(stateCopy).forEach(([key, value]) => {
-            if (key === "downloadLinks") {
-
-                if (Array.isArray(value)) {
-                    const downloadLinksCopy = [...value];
-                    let replacement;
-
-                    downloadLinksCopy.forEach(downloadInfo => {
-                        let encoded = {};
-
-                        if (typeof downloadInfo.link === "string") {
-                            encoded.link = encodeURIComponent(downloadInfo.link.slice());
-                            encoded.linkName = downloadInfo.linkName;
-                        }
-                        else {
-                            encoded = {...downloadInfo};
-                        }
-                        replacement = encoded;
-                        downloadLinks.push(replacement);
-                    });
-                }
-            }
-
-        });
-        return Object.assign(stateCopy, {downloadLinks: downloadLinks});
+        return {
+            layerInfo: state.layerInfo
+        };
     }
 };
--- src_3_0_0/modules/layerInformation/tests/unit/store/gettersLayerInformation.spec.js
+++ src_3_0_0/modules/layerInformation/tests/unit/store/gettersLayerInformation.spec.js
@@ -1,38 +1,18 @@
 import {expect} from "chai";
 import getters from "../../../store/gettersLayerInformation";
-import sinon from "sinon";
 
-describe("src_3_0_0/modules/legend/store/gettersLayerInformation.js", () => {
-
-    afterEach(() => {
-        sinon.restore();
-    });
+describe("src_3_0_0/modules/layerInformation/store/gettersLayerInformation.js", () => {
 
     it("urlParams", () => {
-        const link1 = "https://geodienste.hamburg.de/download?url=https://geodienste.hamburg.de/HH_WFS_EMobility&f=csv",
-            link2 = "https://iot.hamburg.de/v1.1/Things?$filter=Datastreams/properties/serviceName%20eq%20%27HH_STA_E-Ladestationen%27&$count=true&$expand=Locations,Datastreams($expand=Observations($top=3),Sensor,ObservedProperty)",
-            state = {
-                downloadLinks: [
-                    {
-                        link: link1,
-                        linkName: "name 1"
-                    },
-                    {
-                        link: link2,
-                        linkName: "name 2"
-                    }
-                ]
-            },
+        const state = {
+                layerInfo: {
+                    id: "id"
+                }},
             urlParamsState = getters.urlParams(state);
 
         expect(urlParamsState).to.be.an("object");
-        expect(urlParamsState.downloadLinks).to.be.an("array");
-        expect(urlParamsState.downloadLinks.length).to.be.equals(2);
-        expect(urlParamsState.downloadLinks[0].linkName).to.be.equals("name 1");
-        expect(decodeURIComponent(urlParamsState.downloadLinks[0].link)).to.be.equals(link1);
-        expect(urlParamsState.downloadLinks[1].linkName).to.be.equals("name 2");
-        expect(decodeURIComponent(urlParamsState.downloadLinks[1].link)).to.be.equals(link2);
-
+        expect(urlParamsState.layerInfo).to.be.an("object");
+        expect(urlParamsState.layerInfo.id).to.be.equals("id");
     });
 
 });
