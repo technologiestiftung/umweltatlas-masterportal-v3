6377d954661b5565fc227a7e56be27da59dc54ed
Inka Dudek <inka.dudek@gv.hamburg.de>
2024-02-22 13:15:01 +0100

    linter

--- src_3_0_0/modules/layerInformation/tests/unit/store/gettersLayerInformation.spec.js
+++ src_3_0_0/modules/layerInformation/tests/unit/store/gettersLayerInformation.spec.js
@@ -5,22 +5,22 @@ describe("src_3_0_0/modules/layerInformation/store/gettersLayerInformation.js", 
 
     it("urlParams", () => {
         const legendURL = "https://geodienste.hamburg.de/HH_WMS_DOP_belaubt?language=ger&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=dop_belaubt&format=image/png&STYLE=default",
-        url = "https://geodienste.hamburg.de/HH_WMS_DOP_belaubt",
-        state = {
+            url = "https://geodienste.hamburg.de/HH_WMS_DOP_belaubt",
+            state = {
                 layerInfo: {
-                    attributes : undefined,
-                    cswUrl : "https://metaver.de/csw",
-                    customMetadata : undefined,
-                    id : "452",
-                    layername : "Luftbilder DOP 20 (DOP 40 mit Umland)",
-                    legendURL : legendURL,
-                    metaID : "B3008A4D-BA8E-4FDB-8FDD-559D02975BD7",
-                    showDocUrl : undefined,
-                    typ : "WMS",
-                    url : url,
-                    urlIsVisible : true
+                    attributes: undefined,
+                    cswUrl: "https://metaver.de/csw",
+                    customMetadata: undefined,
+                    id: "452",
+                    layername: "Luftbilder DOP 20 (DOP 40 mit Umland)",
+                    legendURL: legendURL,
+                    metaID: "B3008A4D-BA8E-4FDB-8FDD-559D02975BD7",
+                    showDocUrl: undefined,
+                    typ: "WMS",
+                    url: url,
+                    urlIsVisible: true
                 }},
-        urlParamsState = getters.urlParams(state);
+            urlParamsState = getters.urlParams(state);
 
         expect(urlParamsState).to.be.an("object");
         expect(urlParamsState.layerInfo).to.be.an("object");
