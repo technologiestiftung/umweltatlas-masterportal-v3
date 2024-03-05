e837387fefb5cc83374a659ddde4cd931745224c
bruehlca <carina.bruehl@dataport.de>
2024-03-05 14:00:46 +0100

    remove test adjustments

--- src_3_0_0/modules/routing/tests/unit/store/directions/actionsDirections.spec.js
+++ src_3_0_0/modules/routing/tests/unit/store/directions/actionsDirections.spec.js
@@ -403,11 +403,10 @@ describe("src_3_0_0/modules/routing/store/directions/actionsDirections.js", () =
             expect(waypoints[0].getDisplayName()).equal(null);
             expect(waypoints[0].getIndexDirectionsLineString()).equal(null);
             expect(waypoints[0].getFeature().getGeometry().getCoordinates()).deep.to.equal([]);
-            expect(commitSpy.firstCall.args[0]).to.be.equals("setWaypointCoordinates");
-            expect(commitSpy.firstCall.args[1].idx).to.be.equals(0);
-            expect(commitSpy.firstCall.args[1].coordinates).deep.to.be.equal([]);
-            expect(commitSpy.secondCall.args[0]).to.be.equals("setRoutingDirections");
-            expect(commitSpy.secondCall.args[1]).to.be.equals(null);
+
+            expect(commitSpy.args).to.deep.equal([
+                ["setRoutingDirections", null]
+            ]);
         });
 
         it("with more than 2 waypoints", async () => {
