154d6d42debcdc19d9756e6e857b175975037f39
DargeFi <finn-rasmus.darge@dataport.de>
2024-06-27 07:33:40 +0200

    move layerSwiper to shared modules

---
+++ src/shared/modules/layerSwiper/store/stateLayerSwiper.js
@@ -0,0 +1,24 @@
+/**
+ * @typedef {Object}  stateLayerSwiper
+ * @property {Boolean} active Whether the swiper should be active.
+ * @property {Boolean} isMoving Whether the swiper is currently being moved.
+ * @property {HTMLElement} swiper The DOM element for the swiper.
+ * @property {Object} targetLayer The layer that is supposed to be manipulated.
+ * @property {Object} sourceLayer The source layer for comparison.
+ * @property {Number} valueX The current x-axis position of the swiper.
+ * @property {Number} valueY The current y-axis position of the swiper.
+ * @property {String} splitDirection The direction of the swiper ("vertical" or "horizontal").
+ */
+const state = {
+    active: false,
+    currentTimeSliderObject: null,
+    isMoving: false,
+    swiper: null,
+    targetLayer: null,
+    sourceLayer: null,
+    valueX: null,
+    valueY: null,
+    splitDirection: "vertical"
+};
+
+export default state;
---
+++ src/shared/modules/layerSwiper/utils/getPosition.js
@@ -0,0 +1,52 @@
+/**
+ * Returns the new position of the swiper depending on the input event.
+ *
+ * @param {KeyboardEvent.keydown | MouseEvent.mousemove} event DOM Event.
+ * @param {Number} currentPos The current position of the swiper (either x or y).
+ * @param {Number} keyboardMovement Value in pixels that the swiper should be moved when using the arrow keys.
+ * @param {String} direction Direction of movement, either "horizontal" or "vertical".
+ * @returns {Number} The new position of the swiper.
+ */
+export default function getPosition (event, currentPos, keyboardMovement, direction) {
+    let position = currentPos;
+
+    if (event.type === "mousemove") {
+        position = direction === "horizontal" ? event.clientX : event.clientY;
+    }
+    else if (event.type === "keydown") {
+        if (direction === "horizontal") {
+            if (event.key === "ArrowLeft") {
+                position -= keyboardMovement;
+            }
+            else if (event.key === "ArrowRight") {
+                position += keyboardMovement;
+            }
+        }
+        else if (event.key === "ArrowUp") {
+            position -= keyboardMovement;
+        }
+        else if (event.key === "ArrowDown") {
+            position += keyboardMovement;
+        }
+    }
+
+    // If the swiper was moved out of the window, it is set to the border of the window instead
+    if (direction === "horizontal") {
+        if (position < 0) {
+            return 0;
+        }
+        if (position > window.innerWidth) {
+            return window.innerWidth;
+        }
+    }
+    else {
+        if (position < 0) {
+            return 0;
+        }
+        if (position > window.innerHeight) {
+            return window.innerHeight;
+        }
+    }
+
+    return position;
+}
