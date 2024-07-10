4c49952e0b05465c021894a60c3c1ad960bd2d22
Weronika Natur <naturwe-lokal@LGV-W-D6F2227.fhhnet.stadt.hamburg.de>
2024-07-10 14:42:02 +0200

    changed the name of the group baselayer in the legend

--- src/modules/legend/js/layerCollector.js
+++ src/modules/legend/js/layerCollector.js
@@ -3,7 +3,7 @@ import layerCollection from "../../../core/layers/js/layerCollection";
 export default {
     /**
      * Returns all layers of the map and their visibility.
-     * If layer is a group, the grouped subject layers or the first baselayer are returned.
+     * If layer is a group, the grouped subject layers or the first baselayer (with the grouped name) are returned.
      * @returns {Array} - layer holders
      */
     getLayerHolder: () => {
@@ -12,7 +12,10 @@ export default {
         layerCollection.getLayers().forEach(layer => {
             if (layer.get("typ") === "GROUP") {
                 if (layer.get("baselayer")) {
-                    allLayers.push({layer: layer.getLayerSource()[0], visibility: layer.get("visibility")});
+                    const tempLayer = layer;
+
+                    tempLayer.getLayerSource()[0].attributes.name = tempLayer.attributes.name;
+                    allLayers.push({layer: tempLayer.getLayerSource()[0], visibility: tempLayer.get("visibility")});
                 }
                 else {
                     layer.getLayerSource().forEach(groupedLayer => {
