53622e8ec35603123804dbd2af30220151cffccb
Weronika Natur <naturwe-lokal@LGV-W-D6F2227.fhhnet.stadt.hamburg.de>
2024-07-11 16:22:50 +0200

    added global to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -27,7 +27,8 @@ module.exports = [
                 Cesium: true,
                 i18next: true,
                 mapCollection: true,
-                moduleCollection: true
+                moduleCollection: true,
+                StreetSmartApi: true
             }
         },
         plugins: {
