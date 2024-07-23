483ef1f44ef64744f1292e71dae69dd799052c81
Jan Bücher <jan.buecher@wemove.com>
2024-07-23 11:53:51 +0200

    jsdoc

--- src/modules/baselayerSwitcher/store/stateBaselayerSwitcher.js
+++ src/modules/baselayerSwitcher/store/stateBaselayerSwitcher.js
@@ -4,6 +4,7 @@
  * @property {Boolean} activatedExpandable Defines if baselayerSwichter is expandend
  * @property {Object} baselayerIds Ids of the baselayers form config
  * @property {String[]} configPaths Path array of possible config locations. First one found will be used
+ * @property {Boolean} singleBaseLayer Defines if the previous layer is set to hidden
  * @property {Object}  topBaselayerId Id of the baselayer that is on the top (highest zIndex)
  * @property {String} type The type of the baselayerSwitcher component
  */
