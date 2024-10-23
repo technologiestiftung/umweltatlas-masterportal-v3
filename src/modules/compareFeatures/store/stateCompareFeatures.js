34cdf6d7048581a4c0251a081bbda203b25fb691
Weronika Natur <weronika.natur@gv.hamburg.de>
2024-10-23 15:30:39 +0200

    refactor of stateCompareFeatures

--- src/modules/compareFeatures/store/stateCompareFeatures.js
+++ src/modules/compareFeatures/store/stateCompareFeatures.js
@@ -1,22 +1,24 @@
 /**
- * User type definition
- * @typedef {Object} CompareFeaturesState
- * @property {Boolean} active if true, compareFeatures will rendered
- * @property {String} id id of the CompareFeatures component
- * @property {String} name displayed as title (config-param)
- * @property {String} icon icon next to title (config-param)
- * @property {Boolean} renderToWindow if true, tool is rendered in a window, else in sidebar (config-param)
- * @property {Boolean} resizableWindow if true, window is resizable (config-param)
- * @property {Boolean} isVisibleInMenu if true, tool is selectable in menu (config-param)
- * @property {Boolean} deactivateGFI flag if tool should deactivate gfi (config-param)
- * @property {Object} layerFeatures object with the features of a layer
- * @property {String} selectedLayerId the currently selected layer
- * @property {String} currentFeatureName the name of the current Feature
- * @property {Boolean} listFull if true no more features can be added to comparison list
- * @property {Boolean} hasFeatures if true comparison list gets rendered otherwise an infobox shows up
- * @property {Array} layerWithFeaturesToShow Array of the features to a selected layer
- * @property {Boolean} hasMultipleLayers if true multiple layers can be selected within the comparison list
- * @property {Object} preparedList Object with the selected layers and their selected features
+ * State of CompareFeatures.
+ * @module  modules/compareFeatures/store/stateCompareFeatures
+ *
+ * @property {String} [name="common:modules.compareFeatures.title"] Displayed as title (config-param).
+ * @property {Boolean} [hasMouseMapInteractions=false] If this attribute is true, then all other modules will be deactivated when this attribute is also true. Only one module with this attribute true may be open at the same time, since conflicts can arise in the card interactions.
+ * @property {String} [description=""common:modules.compareFeatures.description"] The description that should be shown in the button in the menu.
+ * @property {String} [icon="bi-star"] Icon next to title (config-param).
+ * @property {String[]} [supportedDevices=["Desktop", "Mobile", "Table"]] Devices on which the module is displayed.
+ * @property {String[]} [supportedMapModes=["2D", "3D"]] Map mode in which this module can be used.
+ * @property {String} [type="compareFeatures"] The type of the module.
+ * @property {String} [id="compareFeatures"] Id of the CompareFeatures component.
+ * @property {Object} [layerFeatures={}] Object with the features of a layer.
+ * @property {String} [selectedLayerId=""] Id of currently selected layer.
+ * @property {String} [currentFeatureName=""] The name of the current Feature.
+ * @property {Boolean} [listFull=false] Tf true no more features can be added to comparison list.
+ * @property {Boolean} [hasFeatures=false] Tf true comparison list gets rendered otherwise an infobox shows up.
+ * @property {Array} [layerWithFeaturesToShow=[]] Array of the features to a selected layer.
+ * @property {Boolean} [hasMultipleLayers=false] Tf true multiple layers can be selected within the comparison list.
+ * @property {Object} [preparedList={}] Object with the selected layers and their selected features.
+ * @property {Object} [preparedListDisplayTable={}] Object with the selected layers and their selected features to be displayed in the table.
  */
 const state = {
     // defaults for config.json parameters
