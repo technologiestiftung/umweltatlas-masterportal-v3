09879667ba5383da3b4f91da825e54e78fe4a36c
hhack <hans.f.hack@googlemail.com>
2024-10-25 12:26:26 +0200

    intro modal works

--- src/modules/layerTree/components/LayerStartModal.vue
+++ src/modules/layerTree/components/LayerStartModal.vue
@@ -13,24 +13,65 @@ export default {
     },
     data () {
         return {
-            showTheModal: true
+            uaImgLink: "./resources/img/logo-umweltatlas.svg",
+            showTheModal: true,
+            subjects:[
+                {
+                    title:"Boden",
+                    value:"Boden",
+                    "imgSrc":"./resources/img/welcome-img/fotolia_40771294_subscription_monthly_xxl.jpg",
+                    "copySrc":"© Samiylenko - Forolia.com"
+                },
+                {
+                    title:"Wasser",
+                    value:"Wasser",
+                    "imgSrc":"./resources/img/welcome-img/depositphotos_147902709_xl-2015.jpg",
+                    "copySrc":"© chrissi/ Depositphotos.com"
+                },
+                {
+                    title:"Luft",
+                    value:"Luft",
+                    "imgSrc":"./resources/img/welcome-img/depositphotos_324010502_xl-2015.jpg",
+                    "copySrc":"© yskandag / depositphotos.com"
+                },
+                {
+                    title:"Klima",
+                    value:"Klima",
+                    "imgSrc":"./resources/img/welcome-img/fotolia_26968330_subscription_monthly_l.jpg",
+                    "copySrc":"© alm1982 - Fotolia.com"
+                },                {
+                    title:"Biotope",
+                    value:"Biotope",
+                    "imgSrc":"./resources/img/welcome-img/fotolia_42033806_subscription_monthly_xxl.jpg",
+                    "copySrc":"© photocreo - Fotolia.com"
+                },                {
+                    title:"Nutzung",
+                    value:"Nutzung",
+                    "imgSrc":"./resources/img/welcome-img/depositphotos_182915374_xl-2015_fransz.jpg",
+                    "copySrc":"© depositphotos - fransz"
+                },                {
+                    title:"Verkehr / Lärm",
+                    value:"Verkehr / Lärm",
+                    "imgSrc":"./resources/img/welcome-img/fotolia_46452680_subscription_monthly_xl.jpg",
+                    "copySrc":"© berlinphotos030 - Fotolia.com"
+                },                {
+                    title:"Energie",
+                    value:"Energie",
+                    "imgSrc":"./resources/img/welcome-img/fotolia_35979355_subscription_monthly_xl.jpg",
+                    "copySrc":"© Thorsten Schier - Fotolia.com"
+                },                {
+                    title:"Mensch",
+                    value:"Mensch",
+                    "imgSrc":"./resources/img/welcome-img/depositphotos_15819245_xl-2015.jpg",
+                    "copySrc":"© berlinphotos030 - Fotolia.com"
+                }
+            ]
         };
     },
     computed: {
         ...mapGetters(["allLayerConfigsStructured", "showLayerAddButton", "portalConfig"]),
         ...mapGetters("Modules/LayerTree", ["menuSide"]),
         ...mapGetters("Modules/LayerSelection", {layerSelectionType: "type", layerSelectionName: "name"}),
-        // /**
-        //  * Returns the title of button.
-        //  * @returns {String} the button title.
-        //  */
-        // title () {
-        //     if (typeof this.portalConfig?.tree?.addLayerButton?.buttonTitle === "string" && this.portalConfig?.tree?.addLayerButton?.buttonTitle !== "") {
-        //         return this.portalConfig?.tree?.addLayerButton?.buttonTitle;
-        //     }
-
-        //     return "common:modules.layerTree.addLayer";
-        // }
     },
     methods: {
         ...mapActions("Modules/LayerSelection", ["navigateForward"]),
@@ -45,18 +86,19 @@ export default {
             return sortBy(configs, (conf) => conf.type !== "folder");
         },
         /**
-         * Shows the component LayerSelection and sets it visible.
+         * Listener for click on subject.
+         * @folderName {String} the name of the folder
+         * @idx {number} the index
          * @returns {void}
          */
-        showLayerSelection () {
-            const subjectDataLayerConfs = this.sort(this.allLayerConfigsStructured(treeSubjectsKey)),
-                baselayerConfs = this.allLayerConfigsStructured(treeBaselayersKey);
+        obSubjectSelect(folderName,idx){
+            const subjectDataLayerConfs = this.sort(this.allLayerConfigsStructured(treeSubjectsKey));
+            const subjectDataLayerConfsSubfolder = this.sort(this.allLayerConfigsStructured(treeSubjectsKey))[idx].elements;
             this.changeCurrentComponent({type: this.layerSelectionType, side: this.menuSide, props: {name: this.layerSelectionName}});
-            this.navigateForward({lastFolderName: "root", subjectDataLayerConfs, baselayerConfs});
+            this.navigateForward({lastFolderName: "root", subjectDataLayerConfs});   
+            this.navigateForward({lastFolderName: folderName, subjectDataLayerConfs: this.sort(subjectDataLayerConfsSubfolder)});
             this.setLayerSelectionVisible(true);
-
             this.showTheModal = false
-
         },
         /**
          * Toggles the modal
@@ -64,7 +106,7 @@ export default {
          * @returns {void}
          */
          toggleModal: function (value) {            
-            this.showTheModal = false
+            this.showTheModal = value
         }
     }
 };
@@ -72,24 +114,59 @@ export default {
 
 <template lang="html">
 
-    <div id="start-modal" v-if="showTheModal">
+    <div id="start-modal" v-if="showTheModal" @click="toggleModal(false)">
 
         <div id="start-modal-content">
 
-            <button title="close modal" class="close-button" @click="toggleModal(false)">
+            <button title="close modal" class="close-button btn btn-light" @click="toggleModal(false)">
                 <span class="bootstrap-icon d-sm-none d-md-inline-block">
                     <i class="bi-x-lg"></i>
                 </span>
             </button>
+            <div class="row">
+                <h2 class="mb-2">
+                    <img :src=uaImgLink alt=""/>
+                    Berliner Umweltatlas
+                </h2>
+                <template
+                    v-for="(conf, idx) in subjects"
+                    :key="idx"
+                >
+                    <div class="col-md-4">
+                        <button @click="obSubjectSelect(conf.value,idx)">
+                            <span class="img-wrapper">
+                                <span class="img-text">
+                                    <span
+                                        class="bootstrap-icon d-sm-none d-md-inline-block"
+                                        ><i class="bi-arrow-right-short"></i
+                                    ></span>
+                                    {{conf.title}}
+                                </span>
+                                <img
+                                    loading="lazy"
+                                    :src=conf.imgSrc
+                                />
+                            </span>
+                        </button>
+                        <p class="img-copyright">Bild: {{conf.copySrc}}</p>
+                    </div>
+                </template>
+            </div>
 
-            <button @click="showLayerSelection">
-                LAYERS
-            </button>
+            
+            <p class="info-text mb-0">
+                Die Umweltatlas Map ist die Kartenzentrierte Erweiterung
+                des
+                <a href="https://www.berlin.de/umweltatlas/" target="_blank"
+                    >Umweltatlas</a
+                >
+            </p>
+
             
         </div>
 
     </div>
-    
+
 </template>
 
 <style lang="scss" scoped>
@@ -101,7 +178,7 @@ export default {
         left: 0;
         right: 0;
         bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
+        background-color: rgba(0, 0, 0, 0.7);
         display: flex;
         align-items: center;
         justify-content: center;
@@ -133,4 +210,78 @@ export default {
         cursor: pointer;
     }
 
+    h2 {
+        align-items: center;
+        display: flex;
+        padding-bottom: 15px;
+
+        img {
+            width: 100px;
+            padding-right: 12px;
+        }
+    }
+
+    .row {
+
+        .col-md-4 {
+            margin-bottom: 10px;
+        }
+
+        .img-wrapper {
+            position: relative;
+            height: fit-content;
+            display: inherit;
+
+            .img-text {
+                position: absolute;
+                background-color: black;
+                color: white;
+                font-size: 17px;
+                padding: 3px 10px;
+
+                span {
+                    font-size: 25px;
+                }
+            }
+
+            img {
+                width: 100%;
+                // max-height: 150px;
+            }
+        }
+
+        .img-copyright {
+            text-align: left;
+            font-size: 12px;
+            color: #999;
+        }
+
+        p.info {
+            font-size: 15px;
+            color: #333333;
+        }
+
+        button {
+            // This removes styles added by default to button elements.
+            // For when something should semantically be a button,
+            // but isn't buttony in appearance.
+            background-color: transparent;
+            border: none;
+            margin: 0;
+            padding: 0;
+            text-align: inherit;
+            font: inherit;
+            border-radius: 0;
+            appearance: none; // Just in case we missed anything.
+            border-top: 5px solid black;
+
+        }
+
+        button:hover {
+            border-color: $dark_green;
+            .img-text {
+                background-color: $dark_green;
+            }
+        }
+    }
 </style>
