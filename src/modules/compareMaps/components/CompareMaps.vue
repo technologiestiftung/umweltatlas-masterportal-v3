52d4d85cb9378d5d1f4f9a661e947ccb77f5aae5
hhack <hans.f.hack@googlemail.com>
2024-10-28 09:49:04 +0100

    added compare info sentence

--- src/modules/compareMaps/components/CompareMaps.vue
+++ src/modules/compareMaps/components/CompareMaps.vue
@@ -173,7 +173,6 @@ export default {
 <template>
     <div
         id="compare-maps"
-        class="p-3"
     >
         <div
             v-if="spinnerActive"
@@ -186,6 +185,7 @@ export default {
         </div>
 
         <div :class="{ 'blurred': spinnerActive }">
+            <h5 class="mb-4">{{ $t("common:modules.compareMaps.title") }}</h5>
             <div class="toggle-buttons mb-4">
                 <span class="me-2">{{ $t("common:modules.compareMaps.splitDirection") }}</span>
                 <div class="form-check form-check-inline">
@@ -282,7 +282,6 @@ export default {
 
 #compare-maps {
     position: relative;
-    padding: 1rem;
 
     .overlay {
         position: absolute;
