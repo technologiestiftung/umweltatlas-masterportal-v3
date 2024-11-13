c27ae3dfc7ce481d19f057b0f0ba4403199a2efb
Weronika Natur <weronika.natur@gv.hamburg.de>
2024-11-13 15:17:33 +0100

    i18next moved to before in tests

--- src/shared/modules/table/tests/components/TableComponent.spec.js
+++ src/shared/modules/table/tests/components/TableComponent.spec.js
@@ -9,6 +9,13 @@ config.global.mocks.$t = key => key;
 describe("src/shared/modules/table/components/TableComponent.vue", () => {
     let store;
 
+    before(function () {
+        i18next.init({
+            lng: "cimode",
+            debug: false
+        });
+    });
+
     beforeEach(() => {
         store = createStore({
             namespaced: true,
@@ -26,10 +33,6 @@ describe("src/shared/modules/table/components/TableComponent.vue", () => {
                     }
                 }
             }
-        });
-        i18next.init({
-            lng: "cimode",
-            debug: false
         });
     });
 
