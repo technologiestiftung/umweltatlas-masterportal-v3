d02c374da451a4b641696665435a87c75996d45f
lhumweltatlasberlin <lukas.hartbecke@senstadt.berlin.de>
2025-12-08 17:40:32 +0100

    Update LayerInfoContactButton.vue
    rollback

--- src/modules/layerTree/components/LayerInfoContactButton.vue
+++ src/modules/layerTree/components/LayerInfoContactButton.vue
@@ -36,20 +36,11 @@ export default {
             "topBaselayerId"
         ]),
         /**
-         * Returns a single contact object to use in the contact module.
-         * If pointOfContact / publisher are arrays, the first entry is used.
-         * @returns {Object|null} Contact details.
+         * Returns contact details from pointOfContact if given otherwise from publisher from meta data information.
+         * @returns {String} Contanct details.
          */
         contact () {
-            const primary = Array.isArray(this.pointOfContact)
-                ? this.pointOfContact[0]
-                : this.pointOfContact;
-
-            const fallback = Array.isArray(this.publisher)
-                ? this.publisher[0]
-                : this.publisher;
-
-            return primary || fallback || null;
+            return this.pointOfContact || this.publisher || null;
         },
         /**
          * Returns true if the contact module is configured in either of the menus.
@@ -79,12 +70,7 @@ export default {
         mailOriginHint () {
             const layerInfo = `{"layerInfo":{"id":"${this.layerInfo.id}"}}`;
 
-            return this.$t("common:modules.layerInformation.mailOriginHint") + " <br>" +
-                encodeURI(
-                    window.location.href.toString().split("#")[0] +
-                    `?MENU={"main":{"currentComponent":"layerInformation","attributes":${layerInfo}}}` +
-                    `&LAYERS=[{"id":"${this.layerInfo.id}","visibility":true},{"id":"${this.topBaselayerId}","visibility":true}]`
-                );
+            return this.$t("common:modules.layerInformation.mailOriginHint") + " <br>" + encodeURI(window.location.href.toString().split("#")[0] + `?MENU={"main":{"currentComponent":"layerInformation","attributes":${layerInfo}}}&LAYERS=[{"id":"${this.layerInfo.id}","visibility":true},{"id":"${this.topBaselayerId}","visibility":true}]`);
         }
     },
     methods: {
@@ -94,16 +80,12 @@ export default {
          * @returns {void}
          */
         openContactModule () {
-            if (!this.contact) {
-                return;
-            }
-
             const props = {
                 name: this.$t(this.contactName),
                 to: [
                     {
                         email: this.contact.email,
-                        name: this.contact.name || this.contact.individualName || ""
+                        name: this.contact.name
                     }
                 ],
                 infoMessage: this.infoMessage,
@@ -120,11 +102,12 @@ export default {
         }
     }
 };
+
 </script>
 
 <template lang="html">
     <div
-        id="'layer-component-sub-menu-contact-button' + layerName"
+        id="'layer-component-sub-menu-contact-button' + layerConf.id"
     >
         <FlatButton
             v-if="contactConfigured && contact"
